8be51ffc01e9875c24ff2fa7abf3a7ea150fa6de
David Middlecamp <david@spark.io>
2014-02-20 16:41:36 -0600

    getting ready for release

---
+++ js/bin/spark.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+
+var Interpreter = require(lib + '/interpreter.js');
+var cli = new Interpreter();
+cli.supressWarmupMessages = true;
+cli.startup();
+cli.handle(process.argv);
