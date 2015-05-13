1ee1cc60140420cfb08644642b38db8387b9c227
Bryce Kahle <bkahle@gmail.com>
2015-05-13 11:17:02 -0700

    Fix particle.sh path

--- particle.sh
+++ particle.sh
@@ -3,4 +3,4 @@
 # This project is free software and is licensed under the LGPL
 # You should have received a copy of the GNU Lesser General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-node /spark/particle-cli/app.js $*
+node app.js $*
