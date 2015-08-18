91d65697c1acc5347c6b5e39d0606c273d451ee9
Bryce Kahle <bkahle@gmail.com>
2015-08-17 18:40:02 -0700

    Use networksetup to list available ports and select first Wi-Fi port
    In certain cases `en0` is not a Wi-Fi access point. This uses
    `networksetup -listnetworkserviceorder` so not only will it dynamically
    discover the port name, it will use the configured priority order.
    
    Fixes #48

--- commands/WirelessCommand/connect/darwin.js
+++ commands/WirelessCommand/connect/darwin.js
@@ -1,20 +1,52 @@
 var exec = require('child_process').exec;
 function darwin(opts, cb) {
 
-	var params = 'networksetup -setairportnetwork en0 ' + opts.ssid;
-	if(opts.password) { params += ' ' + opts.password; }
-
-	// TODO: something with opts & interfaces?
-	exec(params, results);
-	function results(err, stdin, stderr) {
-
-		if(err || stderr) {
-
-			// TODO: more research into failure modes of this command
+	exec('networksetup -listnetworkserviceorder', function (err, stdout, stderr) {
+		if (err || stderr) {
 			return cb(err || stderr);
 		}
-		cb(null, opts);
-	};
+
+		var device;
+		var useNextDevice = false;
+		var lines = stdout.split('\n');
+		for (var i=0; i < lines.length; i++) {
+			var line = lines[i];
+			if (!line) {
+				continue;
+			}
+
+			if (useNextDevice) {
+				var searchString = 'Device: ';
+				var idx = line.indexOf(searchString);
+				if (idx > 0) {
+					var lastIndex = line.lastIndexOf(')');
+					device = line.slice(idx + searchString.length, lastIndex).trim();
+					break;
+				} else {
+					return cb(new Error('Unable to parse output of networksetup -listnetworkserviceorder'));
+				}
+			} else if (line.indexOf('Wi-Fi') > 0) {
+				useNextDevice = true;
+				continue;
+			}
+		}
+
+		if (!device) {
+			return cb(new Error('Unable to find a Wi-Fi network interface'));
+		}
+
+		var params = 'networksetup -setairportnetwork ' + device + ' ' + opts.ssid;
+		if (opts.password) { params += ' ' + opts.password; }
+
+		// TODO: something with opts & interfaces?
+		exec(params, function results(err, stdout, stderr) {
+			if (err || stderr) {
+				// TODO: more research into failure modes of this command
+				return cb(err || stderr);
+			}
+			cb(null, opts);
+		});
+	});
 };
 
 module.exports = darwin;
