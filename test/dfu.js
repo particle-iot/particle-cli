e4f878babfa2ae085f93ce4414203eb64d481b96
Bryce Kahle <bkahle@gmail.com>
2015-12-01 18:24:59 -0800

    Add tests around DFU output filtering for device ids

---
+++ test/dfu.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var dfu = require('../lib/dfu');
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+describe('DFU', function() {
+	it('finds Particle devices in dfu-util -l output', function() {
+		var output = fs.readFileSync(path.join(__dirname, './fixtures/dfu/only_particle.txt')).toString();
+		var devices = dfu._deviceIdsFromDfuOutput(output);
+		assert.ok(devices);
+		assert.equal(devices.length, 1);
+		assert.equal(devices[0], '2b04:d006');
+	});
+
+	it('filters out non-Particle devices in dfu-util -l output', function() {
+		var output = fs.readFileSync(path.join(__dirname, './fixtures/dfu/mixed.txt')).toString();
+		var devices = dfu._deviceIdsFromDfuOutput(output);
+		assert.ok(devices);
+		assert.equal(devices.length, 1);
+		assert.equal(devices[0], '2b04:d00a');
+	});
+
+	it('handles no devices output', function() {
+		var output = fs.readFileSync(path.join(__dirname, './fixtures/dfu/none.txt')).toString();
+		var devices = dfu._deviceIdsFromDfuOutput(output);
+		assert.ok(devices);
+		assert.equal(devices.length, 0);
+	});
+});
