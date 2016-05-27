4d0d5b7230fb936c8c3adb3e6de97d3e54235d79
Julien Vanier <jvanier@gmail.com>
2016-05-27 09:48:20 -0400

    Update binaries for 0.5.1

--- settings.js
+++ settings.js
@@ -85,16 +85,16 @@ var settings = {
 	},
 	updates: {
 		'2b04:d006': {
-			systemFirmwareOne: 'system-part1-0.5.0-photon.bin',
-			systemFirmwareTwo: 'system-part2-0.5.0-photon.bin'
+			systemFirmwareOne: 'system-part1-0.5.1-photon.bin',
+			systemFirmwareTwo: 'system-part2-0.5.1-photon.bin'
 		},
 		'2b04:d008': {
-			systemFirmwareOne: 'system-part1-0.5.0-p1.bin',
-			systemFirmwareTwo: 'system-part2-0.5.0-p1.bin'
+			systemFirmwareOne: 'system-part1-0.5.1-p1.bin',
+			systemFirmwareTwo: 'system-part2-0.5.1-p1.bin'
 		},
 		'2b04:d00a': {
-			systemFirmwareOne: 'system-part1-0.5.0-electron.bin',
-			systemFirmwareTwo: 'system-part2-0.5.0-electron.bin'
+			systemFirmwareOne: 'system-part1-0.5.1-electron.bin',
+			systemFirmwareTwo: 'system-part2-0.5.1-electron.bin'
 		}
 	},
 	commandMappings: path.join(__dirname, 'mappings.json')
