df3470411b982690bf4a89223c75c96908e3c271
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-07-03 04:24:39 +0200

    echo command (for testing)

---
+++ src/cmd/echo.js
@@ -0,0 +1,24 @@
+/**
+ * A simple command to echo the passed in options and parameters.
+ */
+
+export default (app, cli) => {
+	cli.createCommand(app, 'echo', 'Echoes the command and options.', {
+		options: {
+			f: {
+				alias: 'flag',
+				boolean: true,
+				description: 'an option'
+			}
+		},
+		params: '[args...]',
+		handler: function echoHandler(argv) {
+			const obj = {
+				cmd: argv._,
+				flag: argv.flag,
+				params: argv.params
+			};
+			console.log(JSON.stringify(obj));
+		}
+	});
+};
--- src/cmd/index.js
+++ src/cmd/index.js
@@ -4,10 +4,12 @@ import help from './help';
 // import event from './event';
 // import alias from './alias';
 
+import echo from './echo';
+
 export default (app, cli) => {
 	// help must come first
 	help(app, cli);
-
+	echo(app, cli);
 	// disable these for now until we have acceptance tests in place
 	// cloud(app, cli);
 	// keys(app, cli);
