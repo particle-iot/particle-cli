f4638e9eee9a4aacc2e596a02c6db5e4c0e10fc9
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-11-28 21:55:32 +0100

    adds library list command

--- src/cli/library_list.js
+++ src/cli/library_list.js
@@ -1,6 +1,9 @@
 import {LibraryListCommand, LibraryListCommandSite} from '../cmd';
 import {convertApiError} from '../cmd/api';
 import {spin} from '../app/ui';
+import {buildAPIClient} from './apiclient';
+import chalk from 'chalk';
+
 
 export class CLILibraryListCommandSite extends LibraryListCommandSite {
 
@@ -8,7 +11,17 @@ export class CLILibraryListCommandSite extends LibraryListCommandSite {
 		super();
 		this._apiClient = apiClient;
 		this.argv = argv;
-		this.sections = argv.sections;
+		if (!argv.sections || !argv.sections.length) {
+			argv.sections = ['official', 'verified', 'popular', 'mine', 'recent'];
+		}
+		// todo - since the text can be used by any app, this could be pushed down to the command layer so it's shared
+		this.headings = {
+			official: 'Official Libraries',
+			verified: 'Verified Libraries',
+			popular: 'Popular Libraries',
+			mine: 'My Libraries',
+			recent: 'Recently added/updated Libraries'
+		}
 	}
 
 	apiClient() {
@@ -17,6 +30,10 @@ export class CLILibraryListCommandSite extends LibraryListCommandSite {
 
 	error(error) {
 		throw convertApiError(error);
+	}
+
+	target() {
+		return {};
 	}
 
 	/**
@@ -29,8 +46,8 @@ export class CLILibraryListCommandSite extends LibraryListCommandSite {
 
 	sections() {
 		const result = {};
-		for (let index of this.sections) {
-			const section = this.sections[index];
+		const sections = this.argv.sections;
+		for (let section of sections) {
 			result[section] = {};
 		}
 		return result;
@@ -39,35 +56,54 @@ export class CLILibraryListCommandSite extends LibraryListCommandSite {
 	notifyFetchLists(promise) {
 		return spin(promise, `Searching for libraries...`)
 			.then((results) => {
-				for (let index in this.sections) {
-					const name = this.sections[index];
+				const sections = this.argv.sections;
+				let separator = false;
+				for (let index in sections) {
+					const name = sections[index];
 					const list = results[name];
 					if (list) {
+						if (separator) {
+							console.log();
+						}
 						this.printSection(name, list);
+						separator = true;
 					}
 				}
 			});
 	}
 
 	printSection(name, libraries) {
-		console.log(name);
-		for (let library of libraries) {
-			this.printLibrary(library);
+		const heading = this.headings[name] || name;
+		console.log(chalk.bold(heading));
+		if (libraries.length) {
+			for (let library of libraries) {
+				this.printLibrary(name, library);
+			}
+		}
+		else {
+			console.log(chalk.grey('   No libraries to show in this section.'));
 		}
 	}
 
-	printLibrary(library) {
-		console.log(library.name);
-		console.log(library.sentence);
-		console.log(library.version);
+	printLibrary(section, library) {
+		console.log(chalk.blue(library.name)+' '+chalk.grey(library.version));
+		let badges = [];
+		if (library.verified) {
+			badges.push(chalk.green('[verified] '));
+		}
+		if (library.visibility==='private') {
+			badges.push(chalk.blue('[private] '));
+		}
+		const badgesText = badges.join('');
+		const defaultSentence = 'no description given';
+		console.log(`   ${badgesText}${library.sentence || defaultSentence}`);
 	}
-
 }
 
 export default ({lib, factory, apiJS}) => {
-	factory.createCommand(lib, 'install', 'installs a library', {
+	factory.createCommand(lib, 'list', 'Lists libraries available', {
 		options: {},
-		params: '[category]',
+		params: '[sections]',
 		handler: function libraryListHandler(argv) {
 			const site = new CLILibraryListCommandSite(argv, buildAPIClient(apiJS));
 			const cmd = new LibraryListCommand();
