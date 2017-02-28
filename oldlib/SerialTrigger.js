7a4296e6496d4d6c41bbf7da884783e9ff7d77b4
Matthew McGowan <mat.mcgowan+github@gmail.com>
2017-02-28 01:59:36 +0000

    update the trigger to allow data to be delivered incrementally. It looks for a trigger that has a prefix matching the current data. if no prefix exists, one character is discarded and the test repeated until a match is found or the data exhausted.

--- oldlib/SerialTrigger.js
+++ oldlib/SerialTrigger.js
@@ -17,33 +17,69 @@ SerialTrigger.prototype.addTrigger = function(prompt, next) {
 		throw new Error('prompt must be specified');
 	}
 	this.triggers[prompt] = next;
+	this.data = '';
 };
 
+/**
+ * There is no guarantee that 'data' events contain all of the data emitted by the device
+ * in one block, so we have to match on substrings.
+ * @param noLogs
+ */
 SerialTrigger.prototype.start = function(noLogs) {
-	var serialDataCallback = function (data) {
-		data = data.toString();
-		var self = this;
-		var triggerFn = _.find(this.triggers, function (fn, prompt) {
-			return data.indexOf(prompt) >= 0;
-		});
 
-		if (triggerFn) {
-			triggerFn(function (response, cb) {
-				if (response) {
-					self.port.flush(function () {
-						self.port.write(response, function () {
-							self.port.drain(function () {
-								if (!noLogs) {
-									log.serialInput(response);
-								}
-								if (cb) {
-									cb();
-								}
+	var serialDataCallback = function (data) {
+		var self = this;
+		this.data += data.toString();
+		var substring = this.data;
+		var substringMatch = '';
+		var matchPrompt = '';
+
+		while (!matchPrompt && substring) {
+			(function(substring) {
+				_.forOwn(self.triggers, function (fn, prompt) {
+					if (substring.length > prompt.length) {
+						if (substring.startsWith(prompt)) {
+							matchPrompt = prompt;
+							return false;   // quit iteration
+						}
+					} else {
+						if (prompt.startsWith(substring)) {
+							matchPrompt = prompt;
+							return false;
+						}
+					}
+				});
+			})(substring);
+
+			if (!matchPrompt) {
+				substring = substring.substring(1);
+			}
+		}
+
+		this.data = substring;
+
+		if (matchPrompt && substring.length >= matchPrompt.length) {
+			this.data = substring.substring(matchPrompt.length);
+
+			var triggerFn = this.triggers[matchPrompt];
+			if (triggerFn) {
+				triggerFn(function (response, cb) {
+					if (response) {
+						self.port.flush(function () {
+							self.port.write(response, function () {
+								self.port.drain(function () {
+									if (!noLogs) {
+										log.serialInput(response);
+									}
+									if (cb) {
+										cb();
+									}
+								});
 							});
 						});
-					});
-				}
-			});
+					}
+				});
+			}
 		}
 	};
 	this.dataCallback = serialDataCallback.bind(this);
