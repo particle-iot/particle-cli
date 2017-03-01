ba58f5bebcf9378e429ce716f1018854562ae11a
Julien Vanier <julien@particle.io>
2017-03-01 16:11:00 -0500

    Update firmware binaries to 0.6.1

--- settings.js
+++ settings.js
@@ -86,18 +86,18 @@ var settings = {
 	},
 	updates: {
 		'2b04:d006': {
-			systemFirmwareOne: 'system-part1-0.6.0-photon.bin',
-			systemFirmwareTwo: 'system-part2-0.6.0-photon.bin'
+			systemFirmwareOne: 'system-part1-0.6.1-photon.bin',
+			systemFirmwareTwo: 'system-part2-0.6.1-photon.bin'
 		},
 		'2b04:d008': {
-			systemFirmwareOne: 'system-part1-0.6.0-p1.bin',
-			systemFirmwareTwo: 'system-part2-0.6.0-p1.bin'
+			systemFirmwareOne: 'system-part1-0.6.1-p1.bin',
+			systemFirmwareTwo: 'system-part2-0.6.1-p1.bin'
 		},
 		'2b04:d00a': {
 			// The bin files MUST be in this order to be flashed to the correct memory locations
-			systemFirmwareOne:   'system-part2-0.6.0-electron.bin',
-			systemFirmwareTwo:   'system-part3-0.6.0-electron.bin',
-			systemFirmwareThree: 'system-part1-0.6.0-electron.bin'
+			systemFirmwareOne:   'system-part2-0.6.1-electron.bin',
+			systemFirmwareTwo:   'system-part3-0.6.1-electron.bin',
+			systemFirmwareThree: 'system-part1-0.6.1-electron.bin'
 		}
 	},
 	commandMappings: path.join(__dirname, 'mappings.json')
