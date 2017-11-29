c09413da244ee7536071aec38eb776f1bd72d2d0
Julien Vanier <julien@particle.io>
2017-11-29 14:05:18 -0500

    Remove babel polyfill

--- bin/particle.js
+++ bin/particle.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 /* eslint no-var: 0 */
 global.verboseLevel = 1;
-require('babel-polyfill');
 var app = appForEnvironment();
 app.default.run(process.argv);
 
 function appForEnvironment() {
 	if (process.env.PARTICLE_CLI_DEVELOPMENT) {
-		require('babel-register');
 		return require('../src/app/app');
 	} else {
 		return require('../dist/app/app');
