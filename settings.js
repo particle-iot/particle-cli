a385a99f1417d9d71a03f616e07123c0c40508f1
Technobly <technobly@gmail.com>
2019-05-14 14:20:31 -0500

    update ascender and Device OS for 1.1.0

--- settings.js
+++ settings.js
@@ -80,20 +80,20 @@ var settings = {
 	},
 	updates: {
 		'2b04:d006': {
-			systemFirmwareOne: 'system-part1-1.0.1-photon-no-boot-dep.bin',
-			systemFirmwareTwo: 'system-part2-1.0.1-photon-no-boot-dep.bin',
-			userFirmware: 'ascender-1.0.1-photon.bin'
+			systemFirmwareOne: 'system-part1-1.1.0-photon-no-boot-dep.bin',
+			systemFirmwareTwo: 'system-part2-1.1.0-photon-no-boot-dep.bin',
+			userFirmware: 'ascender-1.1.0-photon.bin'
 		},
 		'2b04:d008': {
-			systemFirmwareOne: 'system-part1-1.0.1-p1-no-boot-dep.bin',
-			systemFirmwareTwo: 'system-part2-1.0.1-p1-no-boot-dep.bin',
-			userFirmware: 'ascender-1.0.1-p1.bin'
+			systemFirmwareOne: 'system-part1-1.1.0-p1-no-boot-dep.bin',
+			systemFirmwareTwo: 'system-part2-1.1.0-p1-no-boot-dep.bin',
+			userFirmware: 'ascender-1.1.0-p1.bin'
 		},
 		'2b04:d00a': {
 			// The bin files MUST be in this order to be flashed to the correct memory locations
-			systemFirmwareOne:   'system-part2-1.0.1-electron.bin',
-			systemFirmwareTwo:   'system-part3-1.0.1-electron.bin',
-			systemFirmwareThree: 'system-part1-1.0.1-electron.bin'
+			systemFirmwareOne:   'electron-system-part2@1.1.0+debug.bin',
+			systemFirmwareTwo:   'electron-system-part3@1.1.0+debug.bin',
+			systemFirmwareThree: 'electron-system-part1@1.1.0+debug.bin'
 		}
 	},
 };
