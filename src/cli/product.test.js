022a0dc284130f8cfb52b834e1aa0ec4eb1d2d97
busticated <itsbusticated@gmail.com>
2020-01-26 14:01:50 -0800

    add `product device add` subcommand scaffolding

--- src/cli/product.test.js
+++ src/cli/product.test.js
@@ -52,6 +52,7 @@ describe('Product Command-Line Interface', () => {
 					'',
 					'Commands:',
 					'  list  List all devices that are part of a product',
+					'  add   Adds one or more devices into a Product',
 					''
 				].join(os.EOL));
 			});
@@ -93,6 +94,42 @@ describe('Product Command-Line Interface', () => {
 					'  particle product device list 12345                           Lists devices in Product 12345',
 					'  particle product device list 12345 5a8ef38cb85f8720edce631a  Get details for device 5a8ef38cb85f8720edce631a within in product 12345',
 					'  particle product device list 12345 --groups foo bar          Lists devices in Product which are assigned the `foo` or `bar` groups',
+					''
+				].join(os.EOL));
+			});
+		});
+	});
+
+	describe('Handles `product device add` Command', () => {
+		it('Parses arguments', () => {
+			const argv = commandProcessor.parse(root, ['product', 'device', 'add', '12345', '5a8ef38cb85f8720edce631a']);
+			expect(argv.clierror).to.equal(undefined);
+			expect(argv.params).to.eql({ product: '12345', device: '5a8ef38cb85f8720edce631a' });
+		});
+
+		it('Errors when required arguments are missing', () => {
+			const argv = commandProcessor.parse(root, ['product', 'device', 'add']);
+			expect(argv.clierror).to.be.an.instanceof(Error);
+			expect(argv.clierror).to.have.property('message', 'Parameter \'product\' is required.');
+			expect(argv.clierror).to.have.property('data', 'product');
+			expect(argv.clierror).to.have.property('isUsageError', true);
+			expect(argv.params).to.eql({});
+		});
+
+		it('Includes help with examples', () => {
+			const termWidth = null; // don't right-align option type labels so testing is easier
+			commandProcessor.parse(root, ['product', 'device', 'add', '--help'], termWidth);
+			commandProcessor.showHelp((helpText) => {
+				expect(helpText).to.equal([
+					'Adds one or more devices into a Product',
+					'Usage: particle product device add [options] <product> [device]',
+					'',
+					'Options:',
+					'  --file, -f  Path to single column .txt file with list of IDs, S/Ns, IMEIs, or ICCIDs of the devices to add  [string]',
+					'',
+					'Examples:',
+					'  particle product device add 12345 5a8ef38cb85f8720edce631a         Add device id 5a8ef38cb85f8720edce631a into product 12345',
+					'  particle product device add 12345 --file ./path/to/device_ids.txt  Adds a list of devices into product 12345',
 					''
 				].join(os.EOL));
 			});
