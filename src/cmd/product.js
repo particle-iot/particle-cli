e7053e3c37e47c664b16ea7410fe78aed9e7bbf7
busticated <itsbusticated@gmail.com>
2020-01-28 11:31:50 -0800

    add `product device add` command implementation

--- src/cmd/product.js
+++ src/cmd/product.js
@@ -1,5 +1,8 @@
+const os = require('os');
+const fs = require('fs-extra');
 const VError = require('verror');
 const settings = require('../../settings');
+const { errors: { usageError } } = require('../app/command-processor');
 const ParticleAPI = require('./api');
 const { normalizedApiError } = require('../lib/api-client');
 const spinnerMixin = require('../lib/spinner-mixin');
@@ -20,6 +23,70 @@ module.exports = class ProductCommand {
 		spinnerMixin(this);
 	}
 
+	addDevice({ params: { product, device } }){
+		const identifiers = [device];
+		const msg = `Adding device ${device} to product ${product}`;
+		const upload = uploadProductDevices(product, identifiers);
+		return this.showBusySpinnerUntilResolved(msg, upload)
+			.then(result => this.showDeviceAddResult(result));
+	}
+
+	addDevices({ file, params: { product, device } }){
+		if (!device && !file){
+			throw usageError(
+				'`device` parameter or `--file` option is required'
+			);
+		}
+
+		if (device){
+			return this.addDevice({ params: { product, device } });
+		}
+
+		const msg = `Adding devices in ${file} to product ${product}`;
+		const upload = readDeviceListFile(file).then((identifiers) => uploadProductDevices(product, identifiers));
+		return this.showBusySpinnerUntilResolved(msg, upload)
+			.then(result => this.showDeviceAddResult(result));
+	}
+
+	showDeviceAddResult({ product, identifiers, status: { invalidDeviceIds = [], nonmemberDeviceIds = [] } } = {}){
+		identifiers = filterDeviceIdentifiers(identifiers, invalidDeviceIds, nonmemberDeviceIds);
+		const message = [];
+
+		if (identifiers.length){
+			message.push(
+				'Success!',
+				'',
+				`Product ${product} Includes:`,
+				dedupeAndStringifyIDList(identifiers),
+				''
+			);
+		}
+
+		// TODO (mirande): it seems the API never returns this? currently
+		// adding a device id which is already in another product yields
+		// an api response flagging that id as `invalidDeviceIds`
+		if (nonmemberDeviceIds.length){
+			message.push(
+				'Skipped Non-Member IDs:',
+				dedupeAndStringifyIDList(nonmemberDeviceIds),
+				''
+			);
+		}
+
+		if (invalidDeviceIds.length){
+			message.push(
+				'Skipped Invalid IDs:',
+				dedupeAndStringifyIDList(invalidDeviceIds),
+				''
+			);
+		}
+
+		if (!identifiers.length){
+			throw new Error(message.join(os.EOL));
+		}
+		return this.ui.write(message.join(os.EOL));
+	}
+
 	showDeviceDetail({ json, params: { product, device } }){
 		const msg = `Fetching device ${device} detail`;
 		const fetchData = createAPI().getDeviceAttributes(device, product);
@@ -27,7 +94,7 @@ module.exports = class ProductCommand {
 			.then(res => {
 				if (json){
 					this.ui.stdout.write(
-						this.createJSONResult(null, res)
+						createJSONResult(null, res)
 					);
 				} else {
 					this.ui.logDeviceDetail(res);
@@ -35,7 +102,7 @@ module.exports = class ProductCommand {
 			})
 			.catch(error => {
 				const message = 'Error showing product device detail';
-				throw this.createErrorResult({ error, message, json });
+				throw createAPIErrorResult({ error, message, json });
 			});
 	}
 
@@ -49,7 +116,7 @@ module.exports = class ProductCommand {
 			.then(res => {
 				if (json){
 					this.ui.stdout.write(
-						this.createJSONResult(page, res.devices)
+						createJSONResult(page, res.devices)
 					);
 				} else {
 					this.ui.logDeviceDetail(res.devices);
@@ -57,22 +124,8 @@ module.exports = class ProductCommand {
 			})
 			.catch(error => {
 				const message = 'Error listing product devices';
-				throw this.createErrorResult({ error, message, json });
+				throw createAPIErrorResult({ error, message, json });
 			});
-	}
-
-	createJSONResult(page, data){
-		const meta = typeof page === 'number'
-			? { previous: page - 1, current: page, next: page + 1 }
-			: {};
-
-		return new JSONResult(meta, data).toString();
-	}
-
-	createErrorResult({ error: e, message, json }){
-		const error = new VError(normalizedApiError(e), message);
-		error.asJSON = json;
-		return error;
 	}
 };
 
@@ -83,3 +136,72 @@ function createAPI(){
 		accessToken: settings.access_token
 	});
 }
+
+function createJSONResult(page, data){
+	const meta = typeof page === 'number'
+		? { previous: page - 1, current: page, next: page + 1 }
+		: {};
+
+	return new JSONResult(meta, data).toString();
+}
+
+function createAPIErrorResult({ error: e, message, json }){
+	const error = new VError(normalizedApiError(e), message);
+	error.asJSON = json;
+	return error;
+}
+
+function uploadProductDevices(product, identifiers){
+	const file = Buffer.from(identifiers.join('\n'), 'utf8');
+
+	return createAPI()
+		.addDeviceToProduct(null, product, file)
+		.then(status => ({ product, identifiers, status }))
+		.catch(error => {
+			const message = 'Error adding device(s) to product';
+			throw createAPIErrorResult({ error, message, json: false });
+		});
+}
+
+function readDeviceListFile(file){
+	return fs.readFile(file, 'utf8')
+		.then((data) => {
+			return data
+				.split(/\r?\n/)
+				.map(l => l.trim())
+				.filter(l => !!l);
+		})
+		.then(identifiers => {
+			if (!identifiers.length){
+				throw new Error(`${file} is empty`);
+			}
+			return identifiers;
+		})
+		.catch(error => {
+			if (error.code === 'ENOENT'){
+				throw new Error(`${file} does not exist`);
+			}
+			throw error;
+		});
+}
+
+function filterDeviceIdentifiers(identifiers, invalid = [], nonmember = []){
+	identifiers = Array.isArray(identifiers) ? identifiers : [identifiers];
+	return identifiers.reduce((out, x) => {
+		if (!hasDeviceIdentifier(x, invalid) && !hasDeviceIdentifier(x, nonmember)){
+			out.push(x);
+		}
+		return out;
+	}, []);
+}
+
+function hasDeviceIdentifier(x = '', ids = []){
+	return ids
+		.map(id => id.toLowerCase().trim())
+		.some(id => id === x.toLowerCase().trim());
+}
+
+function dedupeAndStringifyIDList(array){
+	return `  ${[...new Set(array)].join(`${os.EOL}  `)}`;
+}
+
