672b56c9f2747debb73393288f51b499cd572df9
busticated <itsbusticated@gmail.com>
2020-02-09 09:32:27 -0800

    variable commands accept cli args object, use `device` param name

--- src/cmd/variable.test.js
+++ src/cmd/variable.test.js
@@ -13,11 +13,11 @@ describe('Variable Command', () => {
 	describe('Monitoring a variable', () => {
 		it('Monitors a variable', withConsoleStubs(sandbox, async () => {
 			const cmd = new VariableCommand();
-			const deviceID = '000000000000000xdeadbeef';
+			const device = '000000000000000xdeadbeef';
 			const variableName = 'test';
 
 			sandbox.stub(cmd, '_pollForVariable').resolves();
-			await cmd.monitorVariables(deviceID, variableName);
+			await cmd.monitorVariables({ params: { device, variableName } });
 
 			expect(process.stdout.write).to.have.callCount(0);
 			expect(process.stderr.write).to.have.callCount(1);
@@ -29,7 +29,7 @@ describe('Variable Command', () => {
 
 			const args = cmd._pollForVariable.firstCall.args;
 
-			expect(args[0]).to.eql([deviceID]);
+			expect(args[0]).to.eql([device]);
 			expect(args[1]).to.eql(variableName);
 			expect(args[2]).to.have.property('delay').that.is.a('number');
 			expect(args[2]).to.have.property('time', undefined);
@@ -37,25 +37,25 @@ describe('Variable Command', () => {
 
 		it('Prompts for variable name when it is not provided', withConsoleStubs(sandbox, async () => {
 			const cmd = new VariableCommand();
-			const deviceID = '000000000000000xdeadbeef';
+			const device = '000000000000000xdeadbeef';
 			let args;
 
 			sandbox.stub(cmd, '_pollForVariable').resolves();
 			sandbox.stub(cmd, 'disambiguateGetValue').resolves({
-				deviceIds: [deviceID],
+				deviceIds: [device],
 				variableName: 'prompted'
 			});
-			await cmd.monitorVariables(deviceID);
+			await cmd.monitorVariables({ params: { device } });
 
 			expect(cmd.disambiguateGetValue).to.have.property('callCount', 1);
 			args = cmd.disambiguateGetValue.firstCall.args;
-			expect(args[0]).to.have.property('deviceId', deviceID);
+			expect(args[0]).to.have.property('device', device);
 			expect(args[0]).to.have.property('variableName', undefined);
 			expect(args).to.have.lengthOf(1);
 
 			expect(cmd._pollForVariable).to.have.property('callCount', 1);
 			args = cmd._pollForVariable.firstCall.args;
-			expect(args[0]).to.eql([deviceID]);
+			expect(args[0]).to.eql([device]);
 			expect(args[1]).to.eql('prompted');
 			expect(args[2]).to.have.property('delay').that.is.a('number');
 			expect(args[2]).to.have.property('time', undefined);
@@ -64,14 +64,14 @@ describe('Variable Command', () => {
 
 		it('Throws when error occurs before polling', async () => {
 			const cmd = new VariableCommand();
-			const deviceID = '000000000000000xdeadbeef';
+			const device = '000000000000000xdeadbeef';
 			let error;
 
 			sandbox.stub(cmd, '_pollForVariable').resolves();
 			sandbox.stub(cmd, 'disambiguateGetValue').rejects(new Error('whoops!'));
 
 			try {
-				await cmd.monitorVariables(deviceID);
+				await cmd.monitorVariables({ params: { device } });
 			} catch (e){
 				error = e;
 			}
