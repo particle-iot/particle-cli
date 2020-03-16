ae37506c263f556f5de88d16b075bfa34dcc1e0a
busticated <itsbusticated@gmail.com>
2020-03-16 16:25:16 -0700

    subscribe command accepts args object, uses standard class format and api client

--- src/cmd/subscribe.js
+++ src/cmd/subscribe.js
@@ -1,77 +1,90 @@
+const os = require('os');
 const VError = require('verror');
-const ApiClient = require('../lib/api-client');
+const settings = require('../../settings');
+const spinnerMixin = require('../lib/spinner-mixin');
+const { normalizedApiError } = require('../lib/api-client');
+const { errors: { usageError } } = require('../app/command-processor');
+const ParticleAPI = require('./api');
+const UI = require('../lib/ui');
 
 
 module.exports = class SubscribeCommand {
-	startListening(event, { device, all }) {
-		const api = new ApiClient();
-		api.ensureToken();
+	constructor({
+		stdin = process.stdin,
+		stdout = process.stdout,
+		stderr = process.stderr
+	} = {}){
+		this.stdin = stdin;
+		this.stdout = stdout;
+		this.stderr = stderr;
+		this.ui = new UI({ stdin, stdout, stderr });
+		spinnerMixin(this);
+	}
 
+	startListening({ device, all, product, params: { event } }){
 		// legacy argument order
 		let eventName = event[0];
 		let deviceId = event[1] || device;
 
 		// if they typed: "particle subscribe mine"
-		if (eventName === 'mine') {
+		if (eventName === 'mine'){
 			eventName = null;
 		}
 
 		let eventLabel = eventName;
-		if (eventLabel) {
+		if (eventLabel){
 			eventLabel = '"' + eventLabel + '"';
 		} else {
 			eventLabel = 'all events';
 		}
 
-		if (!deviceId && !all) {
+		if (!deviceId && !all){
 			deviceId = 'mine';
 		}
 
-		if (!deviceId) {
-			console.log('Subscribing to ' + eventLabel + ' from the firehose (all devices) and my personal stream (my devices)');
-		} else if (deviceId === 'mine') {
-			console.log('Subscribing to ' + eventLabel + ' from my personal stream (my devices only) ');
+		if (product){
+			if (!device){
+				throw usageError(
+					'`device` parameter is required when `--product` flag is set'
+				);
+			}
+		}
+
+		if (!deviceId){
+			this.ui.stdout.write(`Subscribing to ${eventLabel} from the firehose (all devices) and my personal stream (my devices)${os.EOL}`);
+		} else if (deviceId === 'mine'){
+			this.ui.stdout.write(`Subscribing to ${eventLabel} from my personal stream (my devices only)${os.EOL}`);
 		} else {
-			console.log('Subscribing to ' + eventLabel + ' from ' + deviceId + "'s stream");
+			this.ui.stdout.write(`Subscribing to ${eventLabel} from ${deviceId}'s stream${os.EOL}`);
 		}
 
-		let chunks = [];
-		function appendToQueue(arr) {
-			for (let i = 0; i < arr.length; i++) {
-				const line = (arr[i] || '').trim();
-				if (!line) {
-					continue;
-				}
-				chunks.push(line);
-				if (line.indexOf('data:') === 0) {
-					processItem(chunks);
-					chunks = [];
-				}
-			}
-		}
-
-		function processItem(arr) {
-			const obj = {};
-			for (let i=0;i<arr.length;i++) {
-				let line = arr[i];
-
-				if (line.indexOf('event:') === 0) {
-					obj.name = line.replace('event:', '').trim();
-				} else if (line.indexOf('data:') === 0) {
-					line = line.replace('data:', '');
-					Object.assign(obj, JSON.parse(line));
-				}
-			}
-
-			console.log(JSON.stringify(obj));
-		}
-
-		return api.getEventStream(eventName, deviceId, (event) => {
-			const chunk = event.toString();
-			appendToQueue(chunk.split('\n'));
-		}).catch(err => {
-			throw new VError(api.normalizedApiError(err), 'Error subscribing to event stream');
-		});
+		const msg = 'Fetching event stream...';
+		const fetchStream = createAPI().getEventStream(deviceId, eventName, product);
+		const onEvent = (event) => this.ui.stdout.write(`${JSON.stringify(event)}${os.EOL}`);
+		return this.showBusySpinnerUntilResolved(msg, fetchStream)
+			.then(stream => {
+				this.ui.stdout.write(os.EOL);
+				return stream;
+			})
+			.then(stream => stream.on('event', onEvent))
+			.catch(error => {
+				const message = 'Error fetching event stream';
+				throw createAPIErrorResult({ error, message });
+			});
 	}
 };
+
+
+// UTILS //////////////////////////////////////////////////////////////////////
+function createAPI(){
+	return new ParticleAPI(settings.apiUrl, {
+		accessToken: settings.access_token
+	});
+}
+
+function createAPIErrorResult({ error: e, message, json }){
+	const error = new VError(normalizedApiError(e), message);
+	error.asJSON = json;
+	return error;
+}
 
