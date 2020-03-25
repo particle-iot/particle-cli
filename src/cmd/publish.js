7bcd7f7ed43a53823e07ac4a3cbf4cb40fbd1421
busticated <itsbusticated@gmail.com>
2020-03-25 15:19:52 -0700

    publish command accepts args object, uses standard class format and api client

--- src/cmd/publish.js
+++ src/cmd/publish.js
@@ -1,23 +1,50 @@
+const os = require('os');
 const VError = require('verror');
-const ApiClient = require('../lib/api-client');
-const ensureError = require('../lib/utilities').ensureError;
+const settings = require('../../settings');
+const spinnerMixin = require('../lib/spinner-mixin');
+const { normalizedApiError } = require('../lib/api-client');
+const ParticleAPI = require('./api');
+const UI = require('../lib/ui');
 
 
 module.exports = class PublishCommand {
-	constructor(options) {
-		this.options = options;
+	constructor({
+		stdin = process.stdin,
+		stdout = process.stdout,
+		stderr = process.stderr
+	} = {}){
+		this.stdin = stdin;
+		this.stdout = stdout;
+		this.stderr = stderr;
+		this.ui = new UI({ stdin, stdout, stderr });
+		spinnerMixin(this);
 	}
 
-	publishEvent(eventName, data, { 'public': publicFlag, 'private': privateFlag }) {
-		// Cannot use usual destructuring since public and private are reserved keywords
-		const setPrivate = publicFlag ? false : privateFlag;
-
-		const api = new ApiClient();
-		api.ensureToken();
-
-		return api.publishEvent(eventName, data, setPrivate).catch((err) => {
-			throw new VError(ensureError(err), 'Could not publish event');
-		});
+	publishEvent({ private: isPrivate, public: isPublic, params: { event, data } }){
+		const setPrivate = isPublic ? false : isPrivate;
+		const visibility = setPrivate ? 'private' : 'public';
+		const epilogue = `${visibility} event: ${event}`;
+		const publishEvent = createAPI().publishEvent(event, data, setPrivate);
+		return this.showBusySpinnerUntilResolved(`Publishing ${epilogue}`, publishEvent)
+			.then(() => this.ui.stdout.write(`Published ${epilogue}${os.EOL}${os.EOL}`))
+			.catch(error => {
+				const message = 'Error publishing event';
+				throw createAPIErrorResult({ error, message });
+			});
 	}
 };
+
+
+// UTILS //////////////////////////////////////////////////////////////////////
+function createAPI(){
+	return new ParticleAPI(settings.apiUrl, {
+		accessToken: settings.access_token
+	});
+}
+
+function createAPIErrorResult({ error: e, message, json }){
+	const error = new VError(normalizedApiError(e), message);
+	error.asJSON = json;
+	return error;
+}
 
