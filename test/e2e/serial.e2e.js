ca1b84850cde6b575bd19665d6bfd976ca93c51f
busticated <itsbusticated@gmail.com>
2020-05-07 14:21:45 -0700

    add e2e tests for `serial mac` and `serial monitor` commands, tune test grouping

--- test/e2e/serial.e2e.js
+++ test/e2e/serial.e2e.js
@@ -1,9 +1,11 @@
+const os = require('os');
 const words = require('lodash/words');
 const capitalize = require('lodash/capitalize');
 const { expect } = require('../setup');
 const cli = require('../lib/cli');
 const {
 	DEVICE_ID,
+	DEVICE_NAME,
 	DEVICE_PLATFORM_ID,
 	DEVICE_PLATFORM_NAME
 } = require('../lib/env');
@@ -31,12 +33,13 @@ describe('Serial Commands [@device]', () => {
 	];
 
 	before(async () => {
-		await cli.startListeningMode();
+		await cli.setTestProfileAndLogin();
+		await cli.flashStrobyFirmwareOTAForTest();
 	});
 
 	after(async () => {
-		await cli.stopListeningMode();
-		await cli.waitUntilOnline();
+		await cli.logout();
+		await cli.setDefaultProfile();
 	});
 
 	it('Shows `help` content', async () => {
@@ -63,35 +66,117 @@ describe('Serial Commands [@device]', () => {
 		expect(exitCode).to.equal(0);
 	});
 
-	it('Lists devices', async () => {
-		const platform = capitalize(DEVICE_PLATFORM_NAME);
-		const { stdout, stderr, exitCode } = await cli.run(['serial', 'list']);
+	describe('Serial List Subcommand', () => {
+		before(async () => {
+			await cli.startListeningMode();
+		});
 
-		expect(stdout).to.include('Found 1 device connected via serial');
-		expect(stdout).to.include(`${platform} - ${DEVICE_ID}`);
-		expect(stderr).to.equal('');
-		expect(exitCode).to.equal(0);
+		after(async () => {
+			await cli.stopListeningMode();
+			await cli.waitUntilOnline();
+		});
+
+		it('Lists devices', async () => {
+			const platform = capitalize(DEVICE_PLATFORM_NAME);
+			const { stdout, stderr, exitCode } = await cli.run(['serial', 'list']);
+
+			expect(stdout).to.include('Found 1 device connected via serial');
+			expect(stdout).to.include(`${platform} - ${DEVICE_ID}`);
+			expect(stderr).to.equal('');
+			expect(exitCode).to.equal(0);
+		});
 	});
 
-	it('Identifies device', async () => {
-		const { stdout, stderr, exitCode } = await cli.run(['serial', 'identify']);
+	describe('Serial Identify Subcommand', () => {
+		before(async () => {
+			await cli.startListeningMode();
+		});
 
-		expect(stdout).to.include(`Your device id is ${DEVICE_ID}`);
-		expect(stdout).to.include('Your system firmware version is');
-		expect(stderr).to.equal('');
-		expect(exitCode).to.equal(0);
+		after(async () => {
+			await cli.stopListeningMode();
+			await cli.waitUntilOnline();
+		});
+
+		it('Identifies device', async () => {
+			const { stdout, stderr, exitCode } = await cli.run(['serial', 'identify']);
+
+			expect(stdout).to.include(`Your device id is ${DEVICE_ID}`);
+			expect(stdout).to.include('Your system firmware version is');
+			expect(stderr).to.equal('');
+			expect(exitCode).to.equal(0);
+		});
 	});
 
-	it('Inspects device', async () => {
-		const platform = capitalize(DEVICE_PLATFORM_NAME);
-		const { stdout, stderr, exitCode } = await cli.run(['serial', 'inspect']);
-		const pass = words(stdout).filter(w => w === 'PASS');
+	describe('Serial Inspect Subcommand', () => {
+		before(async () => {
+			await cli.startListeningMode();
+		});
 
-		expect(stdout).to.not.include('FAIL');
-		expect(stdout).to.include(`Platform: ${DEVICE_PLATFORM_ID} - ${platform}`);
-		expect(pass).to.have.lengthOf.at.least(16);
-		expect(stderr).to.equal('');
-		expect(exitCode).to.equal(0);
+		after(async () => {
+			await cli.stopListeningMode();
+			await cli.waitUntilOnline();
+		});
+
+		it('Inspects device', async () => {
+			const platform = capitalize(DEVICE_PLATFORM_NAME);
+			const { stdout, stderr, exitCode } = await cli.run(['serial', 'inspect']);
+			const pass = words(stdout).filter(w => w === 'PASS');
+
+			expect(stdout).to.not.include('FAIL');
+			expect(stdout).to.include(`Platform: ${DEVICE_PLATFORM_ID} - ${platform}`);
+			expect(pass).to.have.lengthOf.at.least(16);
+			expect(stderr).to.equal('');
+			expect(exitCode).to.equal(0);
+		});
+	});
+
+	describe('Serial Mac Subcommand', () => {
+		before(async () => {
+			await cli.startListeningMode();
+		});
+
+		after(async () => {
+			await cli.stopListeningMode();
+			await cli.waitUntilOnline();
+		});
+
+		it('Gets MAC address from device', async () => {
+			const macAddressPtn = /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+			const { stdout, stderr, exitCode } = await cli.run(['serial', 'mac']);
+			const [address] = stdout.trim().match(macAddressPtn);
+
+			expect(stdout).to.equal(`${os.EOL}Your device MAC address is ${address}`);
+			expect(stderr).to.equal('');
+			expect(exitCode).to.equal(0);
+		});
+	});
+
+	describe('Serial Monitor Subcommand', () => {
+		after(async () => {
+			await cli.callStrobyStop(DEVICE_NAME);
+		});
+
+		it('Monitors serialport output', async () => {
+			await cli.callStrobyStart(DEVICE_NAME);
+			const received = [];
+			const { exitCode } = await cli.waitForResult(['serial', 'monitor'], (data) => {
+				const log = data.toString('utf8');
+
+				received.push(log);
+
+				if (received.length > 5){
+					return true;
+				}
+				return false;
+			});
+			const [msg, status, ...logs] = received.join('').split(/\r?\n/).filter(x => !!x);
+
+			expect(msg).to.include('Opening serial monitor for com port:');
+			expect(status).to.equal('Serial monitor opened successfully:');
+			expect(logs).to.have.lengthOf.above(1);
+			logs.forEach(l => expect(l).to.equal(`${DEVICE_ID.substring(0, 6)} - active`));
+			expect(exitCode).to.equal(0);
+		});
 	});
 });
 
