94e5a1fd19af36d93c3ddbcc3df12888a6aa095e
keeramis <keerthy.amisagadda@gmail.com>
2023-05-11 18:58:59 +0530

    Add tests

--- src/cmd/bundle.js
+++ src/cmd/bundle.js
@@ -12,19 +12,46 @@ module.exports = class BundleCommands extends CLICommandBase {
 		if (!fs.existsSync(appBinary)) {
 			throw new Error('The file ' + appBinary + ' does not exist!');
 		} else {
-			const ext = utilities.getFilenameExt(appBinary).toLowerCase();
+			const ext = utilities.getFilenameExt(appBinary);
 			if (ext !== '.bin') {
 				throw new Error('The file ' + appBinary + ' is not a valid binary');
 			}
 		}
-		const assetsList = fs.readdirSync(assets)
-			.map(f => path.join(assets, f));
-		const bundle = await createApplicationAndAssetBundle(appBinary, assetsList);
-		const downloadFilename = await this._getDownloadBundlePath(saveTo, appBinary);
-		await fs.writeFile(downloadFilename, bundle);
+
+		if (!assets) {
+			// If no assets folder is specified, use the default assets folder
+			assets = path.join(process.cwd(), 'assets');
+		}
+
+		if (!fs.existsSync(assets)) {
+			throw new Error('The folder ' + assets + ' does not exist!');
+		}
+
+		// Gets the assets only from the main folder and any sub-folders are ignored
+		// 'assets' is the folder path of assets to be bundled
+		const assetsInFolder = fs.readdirSync(assets).map(f => path.join(assets, f));
+		if (assetsInFolder.length === 0) {
+			throw new Error('No assets found in ' + assets);
+		}
+
+		const assetsList = assetsInFolder.map(f => { return { data: fs.readFileSync(f), name: path.basename(f) }});
+
+		let downloadFilename;
+		return createApplicationAndAssetBundle(appBinary, assetsList)
+			.then((bundle) => {
+				downloadFilename = this._getDownloadBundlePath(saveTo, appBinary);
+				return fs.writeFile(downloadFilename, bundle);
+			})
+			.then(() => {
+				this.ui.stdout.write(`Success! Downloaded bundle to ${downloadFilename}\n`);
+				return downloadFilename;
+			})
+			.catch(err => {
+				this.ui.stderr.write(err.message);
+			});
 	}
 
-	async _getDownloadBundlePath(saveTo, appBinaryPath) {
+	_getDownloadBundlePath(saveTo, appBinaryPath) {
 		if (saveTo){
 			const ext = utilities.getFilenameExt(saveTo);
 			if (ext === '.zip') {
--- src/cmd/bundle.test.js
+++ src/cmd/bundle.test.js
@@ -1,56 +1,207 @@
-const { expect } = require('../../test/setup');
+const path = require('path');
+const { expect, sinon } = require('../../test/setup');
 const BundleCommands = require('./bundle');
+const fs = require('fs');
+const { PATH_FIXTURES_THIRDPARTY_OTA_DIR, USERNAME } = require('../../test/lib/env');
+const cli = require('../../test/lib/cli');
 
-describe('bundle', () => {
-	beforeEach(() => {
-	});
+describe('BundleCommands', () => {
+	it('should throw an error if the app binary is not provided', () => {
+		const bundleCommands = new BundleCommands();
+		const args = {
+			params: {
+				appBinary: undefined
+			},
+			assets: undefined,
+			saveTo: undefined
+		};
 
-	it('should throw an error if the app binary does not exist', () => {
-		expect(true).to.eq(true);
+		return bundleCommands.createBundle(args)
+			.then(() => {
+				expect(false).to.eq(true);
+			})
+			.catch(error => {
+				expect(error).to.have.property('message', 'The file undefined does not exist!');
+			});
 	});
 
 	it('should throw an error if the app binary is not a valid binary', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const args = {
+			params: {
+				appBinary: 'app.txt'
+			},
+			assets: undefined,
+			saveTo: undefined
+		};
+		// !!! Stub doesn't work here
+		const fsStub = sinon.stub(fs, 'existsSync');
+		fsStub.returns(true);
+
+		return bundleCommands.createBundle(args)
+			.catch(error => {
+				expect(error).to.have.property('message', 'The file app.txt is not a valid binary');
+			})
+			.finally(() => {
+				fsStub.restore();
+			});
 	});
 
 	it('should return a .zip file', () => {
-		expect(true).to.eq(true);
-	});
-
-	it('should throw an error if --assets option is not specified and assets folder is not present', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'assets');
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app.zip'
+		};
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				expect(result).to.include('app.zip');
+			});
 	});
 
 	it('should use the assets in the assets folder if --assets option is not specified but assets folder is present', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = undefined;
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app.zip'
+		};
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				expect(result).to.include('app.zip');
+			})
+			.catch(error => {
+				console.log('Error is : ' + error);
+				expect(false).to.eq(true);
+			});
+	});
+
+	it('should throw an error if --assets option is not specified and assets folder is not present', () => {
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = undefined;
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app.zip'
+		};
+
+		// Same here - stub does not work
+		const fsStub = sinon.stub(fs, 'existsSync');
+		fsStub.withArgs(assetsPath).returns(false);
+
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				expect(false).to.eq(true);
+			})
+			.catch(error => {
+				console.log('Error is : ' + error);
+				expect(error).to.have.property('message', 'The folder undefined does not exist!');
+			})
+			.finally(() => {
+				fsStub.restore();
+			});
 	});
 
 	it('should use the assets in the assets folder if --assets option is specified and assets folder is present', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = undefined;
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app.zip'
+		};
+
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				expect(result).to.include('app.zip');
+			})
+			.catch(error => {
+				console.log('Error is : ' + error);
+				expect(false).to.eq(true);
+			});
 	});
 
-	it('should still bundle is assets folder is empty', () => {
-		expect(true).to.eq(true);
+	it('return error if there are no assets in the assets folder', () => {
+
+
+
 	});
 
 	it('should return a .zip file with the name given by user if saveTo argument has .zip extension', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'assets');
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app.zip'
+		};
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				expect(result).to.include('app.zip');
+			});
 	});
 
 	it('should return a .zip file with the default name if saveTo argument does not have .zip extension', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'assets');
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: 'app'
+		};
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				// Use regex because of the time mismatch between the test and
+				// the actual execution of the following line
+				expect(result).to.match(/^bundle_app_\d+\.zip$/);
+			});
 	});
 
 	it('should return a .zip file with the default name if saveTo argument is not provided', () => {
-		expect(true).to.eq(true);
-	});
-
-	it('should create a bundle of the application binary and assets', () => {
-		expect(true).to.eq(true);
-	});
-
-	it('should create a bundle of the application binary and assets and save it to a file specified by the user', () => {
-		expect(true).to.eq(true);
+		const bundleCommands = new BundleCommands();
+		const binPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'bin', 'app.bin');
+		const assetsPath = path.join(PATH_FIXTURES_THIRDPARTY_OTA_DIR, 'assets');
+		const args = {
+			params: {
+				appBinary: binPath,
+			},
+			assets: assetsPath,
+			saveTo: undefined
+		};
+		return bundleCommands.createBundle(args)
+			.then(result => {
+				console.log('Result is : ' + result);
+				// Use regex because of the time mismatch between the test and
+				// the actual execution of the following line
+				expect(result).to.match(/^bundle_app_\d+\.zip$/);
+			});
 	});
 
 	it('should return any unexpected errors', () => {
