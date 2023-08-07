7fd0ca9f14bcff1a09c045b92a53e9e3a804cdf7
Hugo Montero <hugo.montero.ventura@particle.io>
2023-08-07 16:56:23 -0600

    add download binaries from device os version function

---
+++ src/lib/device-os-version-util.js
@@ -0,0 +1,86 @@
+const { ensureFolder } = require('../../settings');
+const deviceConstants = require('@particle/device-constants');
+const path = require('path');
+const request = require('request');
+const fs = require('fs-extra');
+
+/**
+ * Download a file from the given url to the given directory
+ * @param url - the url to download from
+ * @param directory - the directory to download to
+ * @param filename - the filename to use
+ * @returns {Promise<unknown>} - a promise that resolves when the file is downloaded
+ */
+function downloadFile(url, directory, filename) {
+	return new Promise(async (resolve, reject) => {
+		filename = filename || url.match(/.*\/(.*)/)[1];
+		console.log('Downloading ' + filename + '...');
+		await fs.ensureDir(directory);
+		let file = fs.createWriteStream(directory + '/' + filename);
+		file.on('finish', () => {
+			file.close(() => {
+				resolve(directory + '/' + filename);
+			});
+		});
+		request
+			.get(url)
+			.pipe(file)
+			.on('error', (err) => {
+				reject(err);
+			});
+	});
+}
+
+/**
+ * Download the binary for the given platform and module
+ * @param api - the api object
+ * @param platformName - the platform name
+ * @param module - the module object to download
+ * @param baseUrl - the base url for the api
+ * @returns {Promise<string>} - the path to the binary
+ */
+const downloadBinary = async ({ platformName, module, baseUrl, version }) => {
+	const basePath = ensureFolder();
+	const binaryPath = path.join(basePath, 'device-os-flash/binaries/', version, platformName);
+	// fetch the binary
+	const uri = `${baseUrl}/${module.filename}`;
+	return downloadFile(uri, binaryPath, module.filename);
+};
+
+/**
+ * Download the binaries for the given platform and version by default the latest version is downloaded
+ * @param {Object} api - the api object
+ * @param {number} platformId - the platform id
+ * @param {string} version - the version to download (default: latest)
+ * @returns {Promise<*[]>} - true if successful
+ */
+const downloadDeviceOsVersionBinaries = async ({ api, platformId, version='latest' }) => {
+	try {
+		const downloadedBinaries = [];
+		// get platform by id from device-constants
+		const platform = Object.values(deviceConstants).filter(p => p.public).find(p => p.id === platformId);
+		// get the device os versions
+		console.log('Getting device os version data for platform: ' + platformId + ' version: ' + version + '...');
+		const deviceOsVersion = await api.getDeviceOsVersions(platformId, version);
+		// download binaries for each module in the device os version
+		for await (const module of deviceOsVersion.modules) {
+			downloadedBinaries.push(await downloadBinary({
+				platformName: platform.name,
+				module,
+				baseUrl: deviceOsVersion.base_url,
+				version: deviceOsVersion.version
+			}));
+		}
+		return downloadedBinaries;
+	} catch (error) {
+		if (error.message.includes('404')) {
+			throw new Error(`Device OS version not found for platform: ${platformId} version: ${version}`);
+		}
+		throw new Error('Error downloading binaries for platform: ' + platformId + ' version: ' + version + ' error: ' + error.message);
+	}
+
+};
+
+module.exports = {
+	downloadDeviceOsVersionBinaries
+};
---
+++ src/lib/device-os-version-util.test.js
@@ -0,0 +1,136 @@
+const { expect } = require('../../test/setup');
+const sinon = require('sinon');
+const fs = require('fs-extra');
+const request = require('request');
+const path = require('path');
+const { downloadDeviceOsVersionBinaries } = require('./device-os-version-util');
+const PATH_TMP_DIR = './tmp';
+
+// stub: request, fs, api
+describe('downloadDeviceOsVersionBinaries', () => {
+	const originalEnv = process.env;
+	beforeEach(() => {
+		process.env = {
+			...originalEnv,
+			home: PATH_TMP_DIR,
+		};
+		fs.ensureDirSync(PATH_TMP_DIR);
+	});
+	afterEach(() => {
+		process.env = originalEnv;
+		fs.removeSync(PATH_TMP_DIR);
+		sinon.restore();
+	});
+	it('should download the binaries for the given platform and version by default the latest version is downloaded', async () => {
+		const expectedPath = path.join(PATH_TMP_DIR, '.particle/device-os-flash/binaries/2.3.1/photon');
+		const api = {
+			getDeviceOsVersions: sinon.stub().resolves({
+				version: '2.3.1',
+				base_url: 'https://api.particle.io/v1/firmware/device-os/v2.3.1',
+				modules: [
+					{ filename: 'photon-bootloader@2.3.1+lto.bin' },
+					{ filename: 'photon-system-part1@2.3.1.bin' }
+				]
+			})
+		};
+		expect(fs.existsSync(expectedPath)).to.be.false;
+		const sinonRequest = sinon.stub(request, 'get').returns({
+			pipe: (res) => {
+				res.emit('finish');
+				return {
+					on: (event, cb) => sinon.stub().callsFake(cb)
+				};
+			}
+		});
+
+		const data = await downloadDeviceOsVersionBinaries({ api, platformId: 6 });
+		expect(api.getDeviceOsVersions).to.have.been.calledWith(6, 'latest');
+		expect(data).to.be.an('array').with.lengthOf(2);
+		expect(sinonRequest.firstCall).to.have.been.calledWith('https://api.particle.io/v1/firmware/device-os/v2.3.1/photon-bootloader@2.3.1+lto.bin');
+		expect(sinonRequest.secondCall).to.have.been.calledWith('https://api.particle.io/v1/firmware/device-os/v2.3.1/photon-system-part1@2.3.1.bin');
+		const files = fs.readdirSync(path.join(PATH_TMP_DIR, '.particle/device-os-flash/binaries/2.3.1/photon'));
+		expect(fs.existsSync(expectedPath)).to.be.true;
+		expect(files).to.be.an('array').with.lengthOf(2);
+		expect(files).to.include('photon-bootloader@2.3.1+lto.bin');
+		expect(files).to.include('photon-system-part1@2.3.1.bin');
+	});
+
+	it('should download the binaries for the given platform and version', async() => {
+		const api = {
+			getDeviceOsVersions: sinon.stub().resolves({
+				version: '2.3.1',
+				base_url: 'https://api.particle.io/v1/firmware/device-os/v2.3.1',
+				modules: [
+					{ filename: 'photon-bootloader@2.3.1+lto.bin' },
+					{ filename: 'photon-system-part1@2.3.1.bin' }
+				]
+			})
+		};
+		sinon.stub(request, 'get').returns({
+			pipe: (res) => {
+				res.emit('finish');
+				return {
+					on: (event, cb) => sinon.stub().callsFake(cb)
+				};
+			}
+		});
+
+		const data = await downloadDeviceOsVersionBinaries({ api, platformId: 6, version: '2.3.1' });
+		expect(api.getDeviceOsVersions).to.have.been.calledWith(6, '2.3.1');
+		expect(data).to.be.an('array').with.lengthOf(2);
+		const files = fs.readdirSync(path.join(PATH_TMP_DIR, '.particle/device-os-flash/binaries/2.3.1/photon'));
+		expect(files).to.be.an('array').with.lengthOf(2);
+		expect(files).to.include('photon-bootloader@2.3.1+lto.bin');
+		expect(files).to.include('photon-system-part1@2.3.1.bin');
+
+	});
+
+	it('should fail if the platform is not supported by the requested version', async()  => {
+		let error;
+		const api = {
+			getDeviceOsVersions: sinon.stub().rejects(new Error('404'))
+		};
+		try {
+			await downloadDeviceOsVersionBinaries({ api, platformId: 6, version: '2.3.1' });
+		} catch (e) {
+			error = e;
+		}
+		expect(error.message).to.equal('Device OS version not found for platform: 6 version: 2.3.1');
+	});
+
+	it('should fail in case of an error', async() => {
+		let error;
+		const api = {
+			getDeviceOsVersions: sinon.stub().resolves({
+				version: '2.3.1',
+				base_url: 'http://url-that-does-not-exist.com',
+				modules: [
+					{ filename: 'photon-bootloader@2.3.1+lto.bin' },
+					{ filename: 'photon-system-part1@2.3.1.bin' }
+				]
+			})
+		};
+		sinon.stub(request, 'get').returns({
+			pipe: (res) => {
+				res.emit('finish');
+				return {
+					on: (event, cb) => {
+						if (event === 'error') {
+							cb(new Error('getaddrinfo ENOTFOUND url-that-does-not-exist.com'));
+						}
+						return sinon.stub().callsFake(cb);
+					}
+				};
+			}
+		});
+
+		try {
+			await downloadDeviceOsVersionBinaries({ api, platformId: 6, version: '2.3.1' });
+		} catch (e) {
+			error = e;
+		}
+		expect(error.message).to.equal('Error downloading binaries for platform: 6 version: 2.3.1 error: getaddrinfo ENOTFOUND url-that-does-not-exist.com');
+		expect(api.getDeviceOsVersions).to.have.been.calledWith(6, '2.3.1');
+		expect(request.get).to.have.been.calledOnce;
+	});
+});
