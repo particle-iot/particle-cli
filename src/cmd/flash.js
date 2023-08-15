92dd4f88b3534e0fe6be6b892afe678e624c1a84
Julien Vanier <julien@particle.io>
2023-08-15 09:49:37 -0400

    Keep public methods at the top

--- src/cmd/flash.js
+++ src/cmd/flash.js
@@ -23,6 +23,135 @@ module.exports = class FlashCommand extends CLICommandBase {
 		super(...args);
 	}
 
+	async flash(device, binary, files, { local, usb, serial, factory, force, target, port, yes, 'application-only': applicationOnly }){
+		if (!device && !binary && !local){
+			// if no device nor files are passed, show help
+			throw usageError('You must specify a device or a file');
+		}
+
+		this.ui.logFirstTimeFlashWarning();
+
+		if (usb){
+			await this.flashDfu({ binary, factory, force });
+		} else if (serial){
+			await this.flashYModem({ binary, port, yes });
+		} else if (local){
+			// TODO: implement local flash
+			// Analyze argument list to determine user intent
+			const { device: deviceIdentifier, files: parsedFiles } = await this._parseLocalFlashArguments({ binary, files });
+			console.log(deviceIdentifier, parsedFiles, applicationOnly);
+			// Get device info
+			const deviceInfo = await this._getDeviceInfo(deviceIdentifier);
+			if (deviceInfo) {
+				const platform = deviceInfo.platform;
+				console.log('connected device', platform.name, deviceInfo.deviceId, deviceInfo.deviceMode, deviceInfo.deviceOsVersion);
+				const preparedFiles = await this._prepareFilesToFlash({
+					binary: parsedFiles.shift(),
+					files: parsedFiles,
+					platform,
+					target });
+				console.log(preparedFiles);
+			} else {
+				throw new Error('No device found.');
+			}
+
+		} else {
+			await this.flashCloud({ device, files, target });
+		}
+
+		this.ui.write('Flash success!');
+	}
+
+	flashCloud({ device, files, target }){
+		const CloudCommands = require('../cmd/cloud');
+		const args = { target, params: { device, files } };
+		return new CloudCommands().flashDevice(args);
+	}
+
+	flashYModem({ binary, port, yes }){
+		const SerialCommands = require('../cmd/serial');
+		return new SerialCommands().flashDevice(binary, { port, yes });
+	}
+
+	flashDfu({ binary, factory, force, requestLeave }){
+		return Promise.resolve()
+			.then(() => dfu.isDfuUtilInstalled())
+			.then(() => dfu.findCompatibleDFU())
+			.then(() => {
+				//only match against knownApp if file is not found
+				let stats;
+
+				try {
+					stats = fs.statSync(binary);
+				} catch (error){
+					// file does not exist
+					binary = dfu.checkKnownApp(binary);
+
+					if (binary === undefined){
+						throw new Error(`file does not exist and no known app found. tried: \`${error.path}\``);
+					}
+					return;
+				}
+
+				if (!stats.isFile()){
+					throw new Error('You cannot flash a directory over USB');
+				}
+			})
+			.then(() => {
+				const parser = new ModuleParser();
+				return parser.parseFile(binary)
+					.catch(err => {
+						throw new VError(ensureError(err), `Could not parse ${binary}`);
+					});
+			})
+			.then(info => {
+				if (info.suffixInfo.suffixSize === 65535){
+					this.ui.write('warn: unable to verify binary info');
+					return;
+				}
+
+				if (!info.crc.ok && !force){
+					throw new Error('CRC is invalid, use --force to override');
+				}
+
+				const specs = deviceSpecs[dfu.dfuId];
+				if (info.prefixInfo.platformID !== specs.productId && !force){
+					throw new Error(`Incorrect platform id (expected ${specs.productId}, parsed ${info.prefixInfo.platformID}), use --force to override`);
+				}
+
+				let segmentName;
+				if (factory) {
+					if (info.prefixInfo.moduleFunction !== ModuleInfo.FunctionType.USER_PART) {
+						throw new Error('Cannot flash a non-application binary to the factory reset location');
+					}
+					segmentName = 'factoryReset';
+					if (!specs[segmentName]) {
+						throw new Error('The platform does not support a factory reset application');
+					}
+				}
+
+				if (requestLeave === undefined) {
+					// todo - leave on factory firmware write too?
+					requestLeave = (!factory && info.prefixInfo.moduleFunction === ModuleInfo.FunctionType.USER_PART);
+				}
+
+				return dfu.writeModule(binary, { segmentName, leave: requestLeave });
+			})
+			.catch((err) => {
+				throw new VError(ensureError(err), 'Error writing firmware');
+			});
+	}
+
+	async _getDeviceInfo() {
+		const device = await getOneUsbDevice();
+
+		return {
+			id: device.id,
+			platformId: device.platformId,
+			version: device.firmwareVersion,
+			isInDfuMode: device.isInDfuMode
+		};
+	}
 	// Should be part fo CLICommandBase??
 	_particleApi() {
 		const auth = settings.access_token;
@@ -43,6 +172,7 @@ module.exports = class FlashCommand extends CLICommandBase {
 
 		return particleUsb.openDeviceById(devices[0].id);
 	}
+
 	// should be part of usb util?
 	async _getDeviceInfo(deviceIdentifier) {
 		try {
@@ -173,135 +303,6 @@ module.exports = class FlashCommand extends CLICommandBase {
 			throw new VError('Platform not supported');
 		}
 		return platformSpec.knownApps;
-	}
-	async flash(device, binary, files, { local, usb, serial, factory, force, target, port, yes, 'application-only': applicationOnly }){
-		if (!device && !binary && !local){
-			// if no device nor files are passed, show help
-			throw usageError('You must specify a device or a file');
-		}
-
-		this.ui.logFirstTimeFlashWarning();
-
-		if (usb){
-			await this.flashDfu({ binary, factory, force });
-		} else if (serial){
-			await this.flashYModem({ binary, port, yes });
-		} else if (local){
-			// TODO: implement local flash
-			// Analyze argument list to determine user intent
-			const { device: deviceIdentifier, files: parsedFiles } = await this._parseLocalFlashArguments({ binary, files });
-			console.log(deviceIdentifier, parsedFiles, applicationOnly);
-			// Get device info
-			const deviceInfo = await this._getDeviceInfo(deviceIdentifier);
-			if (deviceInfo) {
-				const platform = deviceInfo.platform;
-				console.log('connected device', platform.name, deviceInfo.deviceId, deviceInfo.deviceMode, deviceInfo.deviceOsVersion);
-				const preparedFiles = await this._prepareFilesToFlash({
-					binary: parsedFiles.shift(),
-					files: parsedFiles,
-					platform,
-					target });
-				console.log(preparedFiles);
-			} else {
-				throw new Error('No device found.');
-			}
-
-		} else {
-			await this.flashCloud({ device, files, target });
-		}
-
-		this.ui.write('Flash success!');
-	}
-
-	flashCloud({ device, files, target }){
-		const CloudCommands = require('../cmd/cloud');
-		const args = { target, params: { device, files } };
-		return new CloudCommands().flashDevice(args);
-	}
-
-	flashYModem({ binary, port, yes }){
-		const SerialCommands = require('../cmd/serial');
-		return new SerialCommands().flashDevice(binary, { port, yes });
-	}
-
-	flashDfu({ binary, factory, force, requestLeave }){
-		return Promise.resolve()
-			.then(() => dfu.isDfuUtilInstalled())
-			.then(() => dfu.findCompatibleDFU())
-			.then(() => {
-				//only match against knownApp if file is not found
-				let stats;
-
-				try {
-					stats = fs.statSync(binary);
-				} catch (error){
-					// file does not exist
-					binary = dfu.checkKnownApp(binary);
-
-					if (binary === undefined){
-						throw new Error(`file does not exist and no known app found. tried: \`${error.path}\``);
-					}
-					return;
-				}
-
-				if (!stats.isFile()){
-					throw new Error('You cannot flash a directory over USB');
-				}
-			})
-			.then(() => {
-				const parser = new ModuleParser();
-				return parser.parseFile(binary)
-					.catch(err => {
-						throw new VError(ensureError(err), `Could not parse ${binary}`);
-					});
-			})
-			.then(info => {
-				if (info.suffixInfo.suffixSize === 65535){
-					this.ui.write('warn: unable to verify binary info');
-					return;
-				}
-
-				if (!info.crc.ok && !force){
-					throw new Error('CRC is invalid, use --force to override');
-				}
-
-				const specs = deviceSpecs[dfu.dfuId];
-				if (info.prefixInfo.platformID !== specs.productId && !force){
-					throw new Error(`Incorrect platform id (expected ${specs.productId}, parsed ${info.prefixInfo.platformID}), use --force to override`);
-				}
-
-				let segmentName;
-				if (factory) {
-					if (info.prefixInfo.moduleFunction !== ModuleInfo.FunctionType.USER_PART) {
-						throw new Error('Cannot flash a non-application binary to the factory reset location');
-					}
-					segmentName = 'factoryReset';
-					if (!specs[segmentName]) {
-						throw new Error('The platform does not support a factory reset application');
-					}
-				}
-
-				if (requestLeave === undefined) {
-					// todo - leave on factory firmware write too?
-					requestLeave = (!factory && info.prefixInfo.moduleFunction === ModuleInfo.FunctionType.USER_PART);
-				}
-
-				return dfu.writeModule(binary, { segmentName, leave: requestLeave });
-			})
-			.catch((err) => {
-				throw new VError(ensureError(err), 'Error writing firmware');
-			});
-	}
-
-	async _getDeviceInfo() {
-		const device = await getOneUsbDevice();
-
-		return {
-			id: device.id,
-			platformId: device.platformId,
-			version: device.firmwareVersion,
-			isInDfuMode: device.isInDfuMode
-		};
 	}
 };
 
