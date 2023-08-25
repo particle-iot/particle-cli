919e7fd312f76741466a23b9538fd979e5c520eb
Hugo Montero <hugo.montero.ventura@particle.io>
2023-08-25 16:12:57 -0600

    refactor flash methods in order to have a helper to share same functions between update and flash

--- src/cmd/update.js
+++ src/cmd/update.js
@@ -1,18 +1,12 @@
 const ParticleApi = require('./api');
-const { platformForId, PLATFORMS } = require('../lib/platform');
-const { delay } = require('../lib/utilities');
+const { platformForId } = require('../lib/platform');
 const settings = require('../../settings');
-
-const { HalModuleParser: ModuleParser, ModuleInfo } = require('binary-version-reader');
 
 const usbUtils = require('./usb-util');
 const deviceOsUtils = require('../lib/device-os-version-util');
 const CLICommandBase = require('./base');
-const { moduleTypeToString, sortBinariesByDependency } = require('../lib/dependency-walker');
-const path = require('path');
-const os = require('os');
+const { parseModulesToFlash, filterModulesToFlash, createFlashSteps, flashFiles } = require('../lib/flash-helper');
 
-const FLASH_APPLY_DELAY = 3000;
 // Flashing an NCP firmware can take a few minutes
 const FLASH_TIMEOUT = 4 * 60000;
 
@@ -48,185 +42,13 @@ module.exports = class UpdateCommand extends CLICommandBase {
 			ui: this.ui,
 			omitUserPart: true
 		});
-		const deviceOsModules = await this._parseModules({ files: deviceOsBinaries });
-		const modulesToFlash = this._filterModulesToFlash({ modules: deviceOsModules, platformId: device.platformId, allowAll: true });
-		const flashSteps = await this._createFlashSteps({ modules: modulesToFlash, isInDfuMode: device.isInDfuMode , platformId: device.platformId });
-		await this._flashFiles({ device, flashSteps });
+		const deviceOsModules = await parseModulesToFlash({ files: deviceOsBinaries });
+		const modulesToFlash = filterModulesToFlash({ modules: deviceOsModules, platformId: device.platformId, allowAll: true });
+		const flashSteps = await createFlashSteps({ modules: modulesToFlash, isInDfuMode: device.isInDfuMode , platformId: device.platformId });
+		await flashFiles({ device, flashSteps, ui: this.ui });
 	}
 
-	async _parseModules({ files }) {
-		return Promise.all(files.map(async (file) => {
-			const parser = new ModuleParser();
-			const binary = await parser.parseFile(file);
-			return {
-				filename: file,
-				...binary
-			};
-		}));
 
-	}
-
-	_filterModulesToFlash({ modules, platformId, allowAll = false }) {
-		const platform = PLATFORMS.find(p => p.id === platformId);
-		const filteredModules = [];
-		// remove encrypted files
-		for (const moduleInfo of modules) {
-			const moduleType = moduleTypeToString(moduleInfo.prefixInfo.moduleFunction);
-			const platformModule = platform.firmwareModules.find(m => m.type === moduleType && m.index === moduleInfo.prefixInfo.moduleIndex);
-			// filter encrypted modules
-			const isEncrypted = platformModule && platformModule.encrypted;
-			const isRadioStack = moduleInfo.prefixInfo.moduleFunction === ModuleInfo.FunctionType.RADIO_STACK;
-			const isNcpFirmware = moduleInfo.prefixInfo.moduleFunction === ModuleInfo.FunctionType.NCP_FIRMWARE;
-			if (!isEncrypted && (!isRadioStack || allowAll) && (!isNcpFirmware || allowAll)) {
-				filteredModules.push(moduleInfo);
-			}
-		}
-		return filteredModules;
-	}
-
-	async _createFlashSteps({ modules, isInDfuMode, platformId }) {
-		const platform = PLATFORMS.find(p => p.id === platformId);
-		const sortedModules = await sortBinariesByDependency(modules);
-		const assetModules = [], normalModules = [], dfuModules = [];
-		sortedModules.forEach(module => {
-			const data = module.prefixInfo.moduleFlags === ModuleInfo.Flags.DROP_MODULE_INFO ? module.fileBuffer.slice(module.prefixInfo.prefixSize) : module.fileBuffer;
-			const flashStep = {
-				name: path.basename(module.filename),
-				moduleInfo: { crc: module.crc, prefixInfo: module.prefixInfo, suffixInfo: module.suffixInfo },
-				data
-			};
-			const moduleType = moduleTypeToString(module.prefixInfo.moduleFunction);
-			const storage = platform.firmwareModules
-				.find(firmwareModule => firmwareModule.type === moduleType);
-			if (moduleType === 'assets') {
-				flashStep.flashMode = 'normal';
-				assetModules.push(flashStep);
-			} else if (moduleType === 'bootloader' || storage.storage === 'external') {
-				flashStep.flashMode = 'normal';
-				normalModules.push(flashStep);
-			} else {
-				flashStep.flashMode = 'dfu';
-				dfuModules.push(flashStep);
-			}
-		});
-
-		// avoid switching to normal mode if device is already in DFU so a device with broken Device OS can get fixed
-		if (isInDfuMode) {
-			return [...dfuModules, ...normalModules, ...assetModules];
-		} else {
-			return [...normalModules, ...dfuModules, ...assetModules];
-		}
-	}
-
-	async _flashFiles({ device, flashSteps }) {
-		const progress = this._createFlashProgress({ flashSteps });
-
-		try {
-			for (const step of flashSteps) {
-				if (step.flashMode === 'normal') {
-					if (device.isInDfuMode) {
-						// put device in normal mode
-						progress({ event: 'switch-mode', mode: 'normal' });
-						device = await usbUtils.reopenInNormalMode(device, { reset: true, timeout: 10000 });
-					}
-					// put the device in listening mode to prevent cloud connection
-					try {
-						await device.enterListeningMode();
-					} catch (error) {
-						// ignore if the device is already in listening mode
-					}
-
-
-					// flash the file in normal mode
-					progress({ event: 'flash-file', filename: step.name });
-					await device.updateFirmware(step.data, { progress });
-
-					// wait for the device to apply the firmware
-					await delay(FLASH_APPLY_DELAY);
-					device = await usbUtils.reopenInNormalMode(device, { reset: false });
-				} else {
-					if (!device.isInDfuMode) {
-						// put device in dfu mode
-						progress({ event: 'switch-mode', mode: 'DFU' });
-						device = await usbUtils.reopenInDfuMode(device);
-					}
-
-					// flash the file over DFU
-					progress({ event: 'flash-file', filename: step.name });
-					// CLI always flashes to internal flash which is the DFU alt setting 0
-					const altSetting = 0;
-					await device.writeOverDfu(step.data, { altSetting, startAddr: parseInt(step.moduleInfo.prefixInfo.moduleStartAddy, 16), progress });
-				}
-			}
-		} finally {
-			progress({ event: 'finish' });
-			await device.reset();
-			await device.close();
-		}
-	}
-
-	_createFlashProgress({ flashSteps }) {
-		const NORMAL_MULTIPLIER = 10; // flashing in normal mode is slower so count each byte more
-		const { isInteractive } = this.ui;
-		let progressBar;
-		if (isInteractive) {
-			progressBar = this.ui.createProgressBar();
-			// double the size to account for the erase and programming steps
-			const total = flashSteps.reduce((total, step) => total + step.data.length * 2 * (step.flashMode === 'normal' ? NORMAL_MULTIPLIER : 1), 0);
-			progressBar.start(total, 0, { description: 'Preparing to flash' });
-		}
-
-		let flashMultiplier = 1;
-		let eraseSize = 0;
-		let step = null;
-		let description;
-		return (payload) => {
-			switch (payload.event) {
-				case 'flash-file':
-					description = `Flashing ${payload.filename}`;
-					if (isInteractive) {
-						progressBar.update({ description });
-					} else {
-						this.ui.stdout.write(`${description}${os.EOL}`);
-					}
-					step = flashSteps.find(step => step.name === payload.filename);
-					flashMultiplier = step.flashMode === 'normal' ? NORMAL_MULTIPLIER : 1;
-					eraseSize = 0;
-					break;
-				case 'switch-mode':
-					description = `Switching device to ${payload.mode} mode`;
-					if (isInteractive) {
-						progressBar.update({ description });
-					} else {
-						this.ui.stdout.write(`${description}${os.EOL}`);
-					}
-					break;
-				case 'erased':
-					if (isInteractive) {
-						// In DFU, entire sectors are erased so the count of bytes can be higher than the actual size
-						// of the file. Ignore the extra bytes to avoid issues with the progress bar
-						if (step && eraseSize + payload.bytes > step.data.length) {
-							progressBar.increment((step.data.length - eraseSize) * flashMultiplier);
-							eraseSize = step.data.length;
-						} else {
-							progressBar.increment(payload.bytes * flashMultiplier);
-							eraseSize += payload.bytes;
-						}
-					}
-					break;
-				case 'downloaded':
-					if (isInteractive) {
-						progressBar.increment(payload.bytes * flashMultiplier);
-					}
-					break;
-				case 'finish':
-					if (isInteractive) {
-						progressBar.stop();
-					}
-					break;
-			}
-		};
-	}
 
 	_particleApi() {
 		const auth = settings.access_token;
---
+++ src/lib/flash-helper.test.js
@@ -0,0 +1,255 @@
+const { expect } = require('../../test/setup');
+const { HalModuleParser, firmwareTestHelper, ModuleInfo, createAssetModule } = require('binary-version-reader');
+const { createFlashSteps, filterModulesToFlash } = require('./flash-helper');
+
+describe('flash-helper', () => {
+	const createModules = async () => {
+		const parser = new HalModuleParser();
+		const preBootloaderBuffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.BOOTLOADER,
+			platformId: 6,
+			moduleIndex: 0,
+			moduleVersion: 1200,
+			deps: []
+		});
+		const preBootloader = await parser.parseBuffer({ fileBuffer: preBootloaderBuffer });
+		const bootloaderBuffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.BOOTLOADER,
+			moduleIndex: 2,
+			platformId: 6,
+			moduleVersion: 1210,
+			deps: [
+				{ func: ModuleInfo.FunctionType.BOOTLOADER, index: 0, version: 1200 }
+			]
+		});
+		const bootloader = await parser.parseBuffer({ fileBuffer: bootloaderBuffer });
+		const systemPart1Buffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.SYSTEM_PART,
+			moduleIndex: 1,
+			platformId: 6,
+			moduleVersion: 4100,
+			deps: [
+				{ func: ModuleInfo.FunctionType.BOOTLOADER, index: 1, version: 1210 }
+			]
+		});
+		const systemPart1 = await parser.parseBuffer({ fileBuffer: systemPart1Buffer });
+		const systemPart2Buffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.SYSTEM_PART,
+			moduleIndex: 2,
+			platformId: 6,
+			moduleVersion: 4100,
+			deps: [
+				{ func: ModuleInfo.FunctionType.SYSTEM_PART, index: 1, version: 4100 }
+			]
+		});
+		const systemPart2 = await parser.parseBuffer({ fileBuffer: systemPart2Buffer });
+		const userPart1Buffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.USER_PART,
+			moduleIndex: 1,
+			platformId: 6,
+			moduleVersion: 4100,
+			deps: [
+				{ func: ModuleInfo.FunctionType.SYSTEM_PART, index: 2, version: 4100 }
+			]
+		});
+		const userPart1 = await parser.parseBuffer({ fileBuffer: userPart1Buffer });
+		return [
+			{ filename: 'preBootloader.bin', ...preBootloader },
+			{ filename: 'bootloader.bin', ...bootloader },
+			{ filename: 'systemPart1.bin', ...systemPart1 },
+			{ filename: 'systemPart2.bin', ...systemPart2 },
+			{ filename: 'userPart1.bin', ...userPart1 }
+		];
+	};
+	const createAssetModules = async() => {
+		const parser = new HalModuleParser();
+		const asset1Buffer = await createAssetModule(Buffer.from('asset1'), 'asset1.txt');
+		const asset1 = await parser.parseBuffer({ fileBuffer: asset1Buffer });
+		const asset2Buffer = await createAssetModule(Buffer.from('asset2'), 'asset2.txt');
+		const asset2 = await parser.parseBuffer({ fileBuffer: asset2Buffer });
+		return [
+			{ filename: 'asset1.bin', ...asset1 },
+			{ filename: 'asset2.bin', ...asset2 }
+		];
+	};
+	const createExtraModules = async () => {
+		const parser = new HalModuleParser();
+		const softDeviceBuffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.RADIO_STACK,
+			moduleIndex: 0,
+			deps: []
+		});
+		const softDevice = await parser.parseBuffer({ fileBuffer: softDeviceBuffer });
+		const ncpBuffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.NCP_FIRMWARE,
+			moduleIndex: 0,
+			deps: []
+		});
+		const ncp = await parser.parseBuffer({ fileBuffer: ncpBuffer });
+		const encryptedModuleBuffer = await firmwareTestHelper.createFirmwareBinary({
+			moduleFunction: ModuleInfo.FunctionType.BOOTLOADER,
+			moduleIndex: 1,
+			deps: []
+		});
+		const encryptedModule = await parser.parseBuffer({ fileBuffer: encryptedModuleBuffer });
+		return {
+			softDevice: { filename: 'softDevice.bin', ...softDevice },
+			ncp: { filename: 'ncp.bin', ...ncp },
+			encryptedModule: { filename: 'encryptedModule.bin', ...encryptedModule }
+		};
+	};
+
+	describe('filterModulesToFlash', () => {
+		let modules, assetModules, extraModules;
+		beforeEach( async () => {
+			modules = await createModules();
+			assetModules = await createAssetModules();
+			extraModules = await createExtraModules();
+		});
+		it('returns modules without ncp, softDevice and encrypted modules', async () => {
+			const filteredModules = filterModulesToFlash({ modules: [...modules, ...assetModules, extraModules.encryptedModule, extraModules.softDevice, extraModules.ncp], platformId: 32 });
+			expect(filteredModules).to.have.lengthOf(7);
+		});
+		it ('returns everything but encrypted modules if allowAll argument is passed', async () => {
+			const filteredModules = filterModulesToFlash({ modules: [...modules, ...assetModules, extraModules.encryptedModule, extraModules.softDevice, extraModules.ncp], platformId: 32, allowAll: true });
+			expect(filteredModules).to.have.lengthOf(9);
+		});
+	});
+
+	describe('createFlashSteps', () => {
+		let preBootloaderStep, bootloaderStep, systemPart1Step, systemPart2Step, userPart1Step, modules, assetModules, asset1Step, asset2Step;
+		beforeEach(async() => {
+			modules = await createModules();
+			assetModules = await createAssetModules();
+			const preBootloader = modules.find( m => m.filename === 'preBootloader.bin');
+			const bootloader = modules.find( m => m.filename === 'bootloader.bin');
+			const systemPart1 = modules.find( m => m.filename === 'systemPart1.bin');
+			const systemPart2 = modules.find( m => m.filename === 'systemPart2.bin');
+			const userPart1 = modules.find( m => m.filename === 'userPart1.bin');
+			const asset1 = assetModules.find( m => m.filename === 'asset1.bin');
+			const asset2 = assetModules.find( m => m.filename === 'asset2.bin');
+			preBootloaderStep = {
+				name: preBootloader.filename,
+				moduleInfo: {
+					crc: preBootloader.crc,
+					prefixInfo: preBootloader.prefixInfo,
+					suffixInfo: preBootloader.suffixInfo
+				},
+				data: preBootloader.fileBuffer,
+				flashMode: 'normal'
+			};
+			bootloaderStep = {
+				name: bootloader.filename,
+				moduleInfo: {
+					crc: bootloader.crc,
+					prefixInfo: bootloader.prefixInfo,
+					suffixInfo: bootloader.suffixInfo
+				},
+				data: bootloader.fileBuffer,
+				flashMode: 'normal'
+			};
+			systemPart1Step = {
+				name: systemPart1.filename,
+				moduleInfo: {
+					crc: systemPart1.crc,
+					prefixInfo: systemPart1.prefixInfo,
+					suffixInfo: systemPart1.suffixInfo
+				},
+				data: systemPart1.fileBuffer,
+				flashMode: 'dfu'
+			};
+			systemPart2Step = {
+				name: systemPart2.filename,
+				moduleInfo: {
+					crc: systemPart2.crc,
+					prefixInfo: systemPart2.prefixInfo,
+					suffixInfo: systemPart2.suffixInfo
+				},
+				data: systemPart2.fileBuffer,
+				flashMode: 'dfu'
+			};
+			userPart1Step = {
+				name: userPart1.filename,
+				moduleInfo: {
+					crc: userPart1.crc,
+					prefixInfo: userPart1.prefixInfo,
+					suffixInfo: userPart1.suffixInfo
+				},
+				data: userPart1.fileBuffer,
+				flashMode: 'dfu'
+			};
+			asset1Step = {
+				name: asset1.filename,
+				moduleInfo: {
+					crc: asset1.crc,
+					prefixInfo: asset1.prefixInfo,
+					suffixInfo: asset1.suffixInfo
+				},
+				data: asset1.fileBuffer,
+				flashMode: 'normal'
+			};
+			asset2Step = {
+				name: asset2.filename,
+				moduleInfo: {
+					crc: asset2.crc,
+					prefixInfo: asset2.prefixInfo,
+					suffixInfo: asset2.suffixInfo
+				},
+				data: asset2.fileBuffer,
+				flashMode: 'normal'
+			};
+		});
+
+		it('returns a list of flash steps', async () => {
+			const steps = await createFlashSteps({
+				modules,
+				platformId: 6,
+				isInDfuMode: false,
+			});
+
+			const expected = [
+				preBootloaderStep,
+				bootloaderStep,
+				systemPart1Step,
+				systemPart2Step,
+				userPart1Step,
+			];
+			expect(steps).to.deep.equal(expected);
+		});
+
+		it('returns first dfu steps if isInDfuMode is true', async () => {
+			const steps = await createFlashSteps({
+				modules,
+				platformId: 6,
+				isInDfuMode: true,
+			});
+
+			const expected = [
+				systemPart1Step,
+				systemPart2Step,
+				userPart1Step,
+				preBootloaderStep,
+				bootloaderStep,
+			];
+			expect(steps).to.deep.equal(expected);
+		});
+
+		it('returns assets at the end of the list', async () => {
+			const steps = await createFlashSteps({
+				modules: [...assetModules, ...modules],
+				platformId: 6,
+				isInDfuMode: false,
+			});
+			const expected = [
+				preBootloaderStep,
+				bootloaderStep,
+				systemPart1Step,
+				systemPart2Step,
+				userPart1Step,
+				asset2Step,
+				asset1Step,
+			];
+			expect(steps).to.deep.equal(expected);
+		});
+	});
+});
