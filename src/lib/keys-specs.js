e6bdd12db900f1ec19330e08835a13b6f8c81614
Julien Vanier <julien@particle.io>
2023-10-02 19:14:00 -0400

    Hotfix for serial commands

---
+++ src/lib/keys-specs.js
@@ -0,0 +1,58 @@
+const keysDctOffsets = {
+	generation1: {
+		tcpServerKey: {
+			address: 0x00001000,
+			size: 2048,
+			format: 'der',
+			alt: 1,
+			alg: 'rsa',
+			addressOffset: 384,
+			portOffset: 450
+		},
+		tcpPrivateKey: {
+			address: 0x00002000,
+			size: 1024,
+			format: 'der',
+			alt: 1,
+			alg: 'rsa'
+		}
+	},
+	laterGenerations: {
+		tcpServerKey: {
+			address: 2082,
+			size: 512,
+			format: 'der',
+			alt: 1,
+			alg: 'rsa',
+			addressOffset: 384,
+			portOffset: 450
+		},
+		udpServerKey: {
+			address: 3298,
+			size: 320,
+			format: 'der',
+			alt: 1,
+			alg: 'ec',
+			addressOffset: 192,
+			portOffset: 258
+		},
+		tcpPrivateKey: {
+			address: 34,
+			size: 612,
+			format: 'der',
+			alt: 1,
+			alg: 'rsa'
+		},
+		udpPrivateKey: {
+			address: 3106,
+			size: 192,
+			format: 'der',
+			alt: 1,
+			alg: 'ec'
+		}
+	}
+};
+
+module.exports = {
+	keysDctOffsets
+};
