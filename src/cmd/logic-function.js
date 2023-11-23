e35d5489b644682c3641037ea44d3068610150af
Hugo Montero <hugo.montero.ventura@particle.io>
2023-11-23 16:13:14 -0600

    implement logic-function create command

--- src/cmd/logic-function.js
+++ src/cmd/logic-function.js
@@ -1,9 +1,13 @@
+const os = require('os');
+const path = require('path');
 const ParticleAPI = require('./api');
+const CLICommandBase = require('./base');
 const VError = require('verror');
 const settings = require('../../settings');
 const { normalizedApiError } = require('../lib/api-client');
-const CLICommandBase = require('./base');
-const os = require('os');
+const { copyAndReplaceTemplate, hasTemplateFiles } = require('../lib/template-processor');
+
+const logicFunctionTemplatePath = __dirname + '/../../assets/logicFunction';
 
 /**
  * Commands for managing encryption keys.
@@ -19,7 +23,7 @@ module.exports = class LogicFunctionsCommand extends CLICommandBase {
 		try {
 			const logicFunctions = await api.getLogicFunctionList({ org });
 			const orgName = getOrgName(org);
-			const list = logicFunctions['logic_functions'];
+			const list = logicFunctions.logic_functions;
 			if (list.length === 0) {
 				this.ui.stdout.write(`No Logic Functions currently deployed in your ${orgName}.`);
 				this.ui.stdout.write(`To create a Logic Function, see \`particle logic-function create\`.${os.EOL}
@@ -35,6 +39,7 @@ module.exports = class LogicFunctionsCommand extends CLICommandBase {
 				});
 				this.ui.stdout.write(`${os.EOL}To view a Logic Function's code, see \`particle lf get.\`${os.EOL}`);
 			}
+			return list;
 		} catch (e) {
 			throw createAPIErrorResult({ error: e, message: 'Error listing logic functions' });
 		}
@@ -46,9 +51,90 @@ module.exports = class LogicFunctionsCommand extends CLICommandBase {
 		console.log(org, name, id);
 	}
 
-	async create({ org, params: { filepath } }) {
-		// TODO
-		console.log(org, filepath);
+	async create({ org, name, params : { filepath } } = { params: { } }) {
+		const orgName = getOrgName(org);
+		const api = createAPI();
+		// get name from filepath
+		if (!filepath) {
+			// use default directory
+			filepath = '.';
+		}
+		if (!name) {
+			const question = {
+				type: 'input',
+				name: 'name',
+				message: 'What would you like to call your Function?'
+			};
+			const result =  await this.ui.prompt([question]);
+			name = result.name;
+		}
+		// ask for description
+		const question = {
+			type: 'input',
+			name: 'description',
+			message: 'Add a description for your Function (optional)'
+		};
+		const result =  await this.ui.prompt([question]);
+		const description = result.description;
+		const slugName = name.toLowerCase().replace(/\s/g, '-');
+		const destinationPath = path.join(filepath, slugName);
+
+		this.ui.stdout.write(`Creating Logic Function ${this.ui.chalk.bold(name)} for ${orgName}...${os.EOL}`);
+		await this._validateExistingName({ api, org, name });
+		await this._validateExistingFiles({ templatePath: logicFunctionTemplatePath, destinationPath });
+
+		const createdFiles = await copyAndReplaceTemplate({
+			templatePath: logicFunctionTemplatePath,
+			destinationPath: path.join(filepath, slugName),
+			replacements: {
+				name: name,
+				description: description || ''
+			}
+		});
+		this.ui.stdout.write(`Successfully created ${this.ui.chalk.bold(name)} in ${this.ui.chalk.bold(filepath)}${os.EOL}`);
+		this.ui.stdout.write(`Files created:${os.EOL}`);
+		createdFiles.forEach((file) => {
+			this.ui.stdout.write(`- ${file}${os.EOL}`);
+		});
+		this.ui.stdout.write(`${os.EOL}Guidelines for creating your Logic Function can be found <TBD>.${os.EOL}`);
+		this.ui.stdout.write(`Once you have written your Logic Function, run${os.EOL}`);
+		this.ui.stdout.write(`- \`particle logic execute\` to run your Function${os.EOL}`);
+		this.ui.stdout.write(`- \`particle logic deploy\` to deploy your new changes${os.EOL}`);
+		return createdFiles;
+	}
+
+	async _validateExistingName({ api, org, name }) {
+		// TODO (hmontero): request for a getLogicFunctionByName() method in the API
+		let existingLogicFunction;
+		try {
+			const logicFunctionsResponse = await api.getLogicFunctionList({ org });
+			const existingLogicFunctions = logicFunctionsResponse.logic_functions;
+			existingLogicFunction = existingLogicFunctions.find((item) => item.name === name);
+		} catch (error) {
+			this.ui.stdout.write(this.ui.chalk.yellow(`Warn: We were unable to check if a Logic Function with name ${name} already exists.${os.EOL}`));
+		}
+		if (existingLogicFunction) {
+			throw new Error(`Error: Logic Function with name ${name} already exists.`);
+		}
+	}
+
+	async _validateExistingFiles({ templatePath, destinationPath }){
+		const filesAlreadyExist = await hasTemplateFiles({
+			templatePath,
+			destinationPath
+		});
+		if (filesAlreadyExist) {
+			const question = {
+				type: 'confirm',
+				name: 'overwrite',
+				message: `We found existing files in ${this.ui.chalk.bold(destinationPath)}. Would you like to overwrite them?`
+			};
+			const { overwrite } =  await this.ui.prompt([question]);
+			if (!overwrite) {
+				this.ui.stdout.write(`Aborted.${os.EOL}`);
+				process.exit(0);
+			}
+		}
 	}
 
 	async execute({ org, data, params: { filepath } }) {
@@ -92,7 +178,7 @@ function createAPIErrorResult({ error: e, message, json }){
 
 // get org name from org slug
 function getOrgName(org) {
-	return org || 'Staging';
+	return org || 'Sandbox';
 }
 
 // TODO (mirande): reconcile this w/ `normalizedApiError()` and `ensureError()`
--- src/cmd/logic-function.test.js
+++ src/cmd/logic-function.test.js
@@ -1,14 +1,17 @@
-const { expect } = require('../../test/setup');
-const LogicFunctionCommands = require('./logic-function');
-const fs = require('fs');
+const os = require('os');
+const fs = require('fs-extra');
 const path = require('path');
 const nock = require('nock');
-const { PATH_FIXTURES_LOGIC_FUNCTIONS } = require('../../test/lib/env');
+const { expect, sinon } = require('../../test/setup');
+const LogicFunctionCommands = require('./logic-function');
+const { PATH_FIXTURES_LOGIC_FUNCTIONS, PATH_TMP_DIR } = require('../../test/lib/env');
+
 
 
 
 describe('LogicFunctionCommands', () => {
 	let logicFunctionCommands;
+	let originalUi = new LogicFunctionCommands().ui;
 	let logicFunc1 = fs.readFileSync(path.join(PATH_FIXTURES_LOGIC_FUNCTIONS, 'logicFunc1.json'), 'utf-8');
 	logicFunc1 = JSON.parse(logicFunc1);
 	let logicFunc2 = fs.readFileSync(path.join(PATH_FIXTURES_LOGIC_FUNCTIONS, 'logicFunc2.json'), 'utf-8');
@@ -16,10 +19,28 @@ describe('LogicFunctionCommands', () => {
 
 	beforeEach(async () => {
 		logicFunctionCommands = new LogicFunctionCommands();
+		logicFunctionCommands.ui = {
+			stdout: {
+				write: sinon.stub()
+			},
+			stderr: {
+				write: sinon.stub()
+			},
+			chalk: {
+				bold: sinon.stub(),
+				cyanBright: sinon.stub(),
+				yellow: sinon.stub(),
+				grey: sinon.stub(),
+			},
+		};
 	});
 
 	afterEach(async () => {
 		// TODO: Fill this out?
+		logicFunctionCommands.ui = originalUi;
+		// remove tmp dir
+		fs.emptyDirSync(PATH_TMP_DIR);
+
 	});
 
 	describe('list', () => {
@@ -27,10 +48,7 @@ describe('LogicFunctionCommands', () => {
 			const stub = nock('https://api.particle.io/v1', )
 				.intercept('/logic/functions', 'GET')
 				.reply(200, logicFunc1);
-			const expectedResp = {
-				body: logicFunc1,
-				statusCode: 200
-			};
+			const expectedResp = logicFunc1.logic_functions;
 
 			const res = await logicFunctionCommands.list({});
 			expect(res).to.eql(expectedResp);
@@ -41,10 +59,7 @@ describe('LogicFunctionCommands', () => {
 			const stub = nock('https://api.particle.io/v1/orgs/particle')
 				.intercept('/logic/functions', 'GET')
 				.reply(200, logicFunc2);
-			const expectedResp = {
-				body: logicFunc2,
-				statusCode: 200
-			};
+			const expectedResp = logicFunc2.logic_functions;
 
 			const res = await logicFunctionCommands.list({ org: 'particle' });
 			expect(res).to.eql(expectedResp);
@@ -55,13 +70,12 @@ describe('LogicFunctionCommands', () => {
 			const stub = nock('https://api.particle.io/v1', )
 				.intercept('/logic/functions', 'GET')
 				.reply(200, { logic_functions: [] });
-			const expectedResp = {
-				body: { logic_functions: [] },
-				statusCode: 200
-			};
+			const expectedResp = [];
 
 			const res = await logicFunctionCommands.list({});
 			expect(res).to.eql(expectedResp);
+			expect(logicFunctionCommands.ui.stdout.write.callCount).to.equal(2);
+			expect(logicFunctionCommands.ui.stdout.write.firstCall.args[0]).to.equal('No Logic Functions currently deployed in your Sandbox.');
 			expect (stub.isDone()).to.be.true;
 		});
 
@@ -97,5 +111,77 @@ describe('LogicFunctionCommands', () => {
 			expect(error).to.be.an.instanceOf(Error);
 			expect(error.message).to.equal('Error listing logic functions: Organization Not Found');
 		});
+	});
+
+	describe('create', () => {
+		it('creates a logic function locally for Sandbox account', async () => {
+			nock('https://api.particle.io/v1', )
+				.intercept('/logic/functions', 'GET')
+				.reply(200, { logic_functions: [] });
+			logicFunctionCommands.ui.prompt = sinon.stub();
+			logicFunctionCommands.ui.prompt.onCall(0).resolves({ name: 'logic func 1' });
+			logicFunctionCommands.ui.prompt.onCall(1).resolves({ description: 'Logic Function 1' });
+			const filePaths = await logicFunctionCommands.create({
+				params: { filepath: PATH_TMP_DIR }
+			});
+			const expectedFiles = ['logic-func-1/code.js', 'logic-func-1/configuration.json'];
+			// Iterate through each file path and check inclusion
+			for (const expectedFile of expectedFiles) {
+				const includesExpected = filePaths.some(value => value.includes(expectedFile));
+				expect(includesExpected, `File path "${expectedFile}" does not include expected values`).to.be.true;
+			}
+		});
+
+		it('shows warning if we cannot look up to cloud for existing logic functions', async () => {
+			nock('https://api.particle.io/v1', )
+				.intercept('/logic/functions', 'GET')
+				.reply(403);
+			logicFunctionCommands.ui.prompt = sinon.stub();
+			logicFunctionCommands.ui.prompt.onCall(0).resolves({ name: 'logicFunc1' });
+			logicFunctionCommands.ui.prompt.onCall(1).resolves({ description: 'Logic Function 1' });
+			await logicFunctionCommands.create({
+				params: { filepath: PATH_TMP_DIR }
+			});
+			expect(logicFunctionCommands.ui.chalk.yellow.callCount).to.equal(1);
+			expect(logicFunctionCommands.ui.chalk.yellow.firstCall.args[0]).to.equal(`Warn: We were unable to check if a Logic Function with name logicFunc1 already exists.${os.EOL}`);
+		});
+
+		it('ask to overwrite if files already exist', async () => {
+			nock('https://api.particle.io/v1', )
+				.intercept('/logic/functions', 'GET')
+				.reply(200, { logic_functions: [] });
+			fs.createFileSync(path.join(PATH_TMP_DIR, 'logicFunc1', 'code.js'));
+			fs.createFileSync(path.join(PATH_TMP_DIR, 'logicFunc1', 'configuration.json'));
+			logicFunctionCommands.ui.prompt = sinon.stub();
+			logicFunctionCommands.ui.prompt.onCall(0).resolves({ name: 'logicFunc1' });
+			logicFunctionCommands.ui.prompt.onCall(1).resolves({ description: 'Logic Function 1' });
+			logicFunctionCommands.ui.prompt.onCall(2).resolves({ overwrite: true });
+			await logicFunctionCommands.create({
+				params: { filepath: PATH_TMP_DIR }
+			});
+			expect(logicFunctionCommands.ui.prompt.callCount).to.equal(3);
+			expect(logicFunctionCommands.ui.prompt.thirdCall.lastArg[0].message).to.contain('We found existing files in');
+		});
+
+		it('throws an error if logic function already exists', async () => {
+			nock('https://api.particle.io/v1', )
+				.intercept('/logic/functions', 'GET')
+				.reply(200, logicFunc1);
+			logicFunctionCommands.ui.prompt = sinon.stub();
+			logicFunctionCommands.ui.prompt.onCall(0).resolves({ name: 'LF1' });
+			logicFunctionCommands.ui.prompt.onCall(1).resolves({ description: 'Logic Function 1' });
+			let error;
+			try {
+				const files = await logicFunctionCommands.create({
+					params: { filepath: PATH_TMP_DIR }
+				});
+				console.log(files);
+			} catch (e) {
+				error = e;
+			}
+			expect(error).to.be.an.instanceOf(Error);
+			expect(error.message).to.equal('Error: Logic Function with name LF1 already exists.');
+		});
+
 	});
 });
