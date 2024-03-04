b95c6539787e8e8dbec4558f8cc61366b7fdb29d
Hugo Montero <hugo.montero.ventura@particle.io>
2024-03-04 17:11:56 -0600

    add integration tests for library init

--- test/integration/library_init.integration.js
+++ test/integration/library_init.integration.js
@@ -17,74 +17,37 @@
  License along with this program; if not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************
  */
-
-const fs = require('fs');
-const path = require('path');
-const mockfs = require('mock-fs');
 const { expect } = require('../setup');
+const fs = require('fs-extra');
+const path = require('path');
 const libraryCommands = require('../../src/cli/library');
-const { LibraryInitCommand } = require('particle-commands');
 const commandProcessor = require('../../src/app/command-processor');
-
-/**
- * Synchronously reads a directory from the real filesystem into the format as expected by mock-fs.
- * @param dir
- */
-function mirrorDir(dir, mock) {
-	if (mock===undefined) {
-		// add the path to the directory (which can be either absolute or relative.)
-		mock = {};
-		mock[dir] = mirrorDir(dir, {});
-		return mock;
-	}
-
-	var files = fs.readdirSync(dir);
-	for (let i in files) {
-		let name = files[i];
-		const fq = path.join(dir, name);
-		if (fs.statSync(fq).isDirectory()) {
-			mock[name] = mirrorDir(fq, {});
-		} else {
-			mock[name] = fs.readFileSync(fq, 'utf-8');
-		}
-	}
-	return mock;
-}
+const { PATH_TMP_DIR } = require('../lib/env');
+const { delay } = require('../lib/mocha-utils');
 
 describe('library init', () => {
-	/**
-	 * The location of the generator resources. This is used to copy the resources to the mock fs
-	 * for the test.
-	 */
-	before(function doit() {
-		this.timeout(20*1000);
-		const initResources = new LibraryInitCommand().generatorClass().sources;
-		const initFixture = mirrorDir(initResources);
-		mockfs(initFixture);
+
+	after(async () => {
+		await fs.remove(path.join(PATH_TMP_DIR, 'lib'));
 	});
 
-	after(() => {
-		mockfs.restore();
-	});
-
-	// This test fails on Node 10 for some reason so I'm disabling it for now
-	xit('can run library init without prompts', function doit() {
-		this.timeout(10*1000);
+	it('can run library init without prompts', async function libraryCreate(){
+		this.timeout(18*1000);
 		const root = commandProcessor.createAppCategory();
 
 		libraryCommands({ commandProcessor, root });
 
+		await fs.ensureDir(path.join(PATH_TMP_DIR, 'lib'));
 		const argv = commandProcessor.parse(root, ['library', 'create', '--name', 'foobar',
-			'--version=1.2.3', '--author=mrbig']);
+			'--version=1.2.3', '--author=mrbig', '--dir', path.join(PATH_TMP_DIR, 'lib')]);
 
 		expect(argv.clicommand).to.be.ok;
 
-		const result = argv.clicommand.exec(argv).then(() => {
-			expect(fs.existsSync('library.properties')).to.equal(true);
-			expect(fs.existsSync('src/foobar.cpp')).to.equal(true);
-			expect(fs.existsSync('src/foobar.h')).to.equal(true);
-		});
-		return result;
+		await argv.clicommand.exec(argv);
+		await delay(1000); // wait for the generator to finish
+		expect(fs.existsSync(path.join(PATH_TMP_DIR, 'lib', 'library.properties'))).to.equal(true);
+		expect(fs.existsSync(path.join(PATH_TMP_DIR, 'lib','src', 'foobar.cpp'))).to.equal(true);
+		expect(fs.existsSync(path.join(PATH_TMP_DIR, 'lib', 'src', 'foobar.h'))).to.equal(true);
 	});
 });
 
