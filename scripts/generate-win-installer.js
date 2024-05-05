0e240ac7501f79dd1dfc28321ae09e0d2498da5f
Hugo Montero <hugo.montero.ventura@particle.io>
2024-05-05 07:02:53 -0600

    add osslsigncode binary to sign win exec

--- scripts/generate-win-installer.js
+++ scripts/generate-win-installer.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const execa = require('execa');
-
+const osslsigncode = process.argv[2] || 'osslsigncode';
 (async () => {
 	// generate ParticleCLISetup installer
 	const args = [
@@ -10,6 +10,6 @@ const execa = require('execa');
 	await fs.move('./installer/windows/ParticleCLISetup.exe', './build/ParticleCLISetup.exe', { overwrite: true });
 	console.log('Generated ParticleCLISetup installer');
 	console.log('Signing Windows Installers');
-	await execa('node', ['./scripts/win-sign.js', 'ParticleCLISetup'], { stdio: 'inherit' });
+	await execa('node', ['./scripts/win-sign.js', 'ParticleCLISetup', osslsigncode], { stdio: 'inherit' });
 	console.log('done');
 })();
