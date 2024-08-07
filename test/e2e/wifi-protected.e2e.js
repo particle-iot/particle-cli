3dbbf8f77aee10b9080c22d105400eb2e563b767
keeramis <keerthy.amisagadda@gmail.com>
2024-08-06 17:19:28 -0700

    lint fix

--- test/e2e/wifi-protected.e2e.js
+++ test/e2e/wifi-protected.e2e.js
@@ -62,37 +62,37 @@ describe.only('Wi-Fi Commands for Protected Devices [@device,@wifi]', () => {
 	});
 
 	describe('WiFi Commands', () => {
-        before(async () => {
-            await cli.run(['wifi', 'clear']);
-        });
+		before(async () => {
+			await cli.run(['wifi', 'clear']);
+		});
 
-        beforeEach(async () => {
-            await cli.run(['device-protection', 'enable']);
-        });
+		beforeEach(async () => {
+			await cli.run(['device-protection', 'enable']);
+		});
 
 		it('Adds a Wi-Fi network', async () => {
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'add', '--file', WIFI_CREDS_FILE]);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(stripAnsi(stdout)).to.include(`Wi-Fi network '${WIFI_SSID}' added successfully.`);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
 		});
 
 		it('Joins a Wi-Fi network', async () => {
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'join', '--file', WIFI_CREDS_FILE]);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(stripAnsi(stdout)).to.include(`Wi-Fi network '${WIFI_SSID}' configured successfully. Attempting to join...\nUse particle wifi current to check the current network.`);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
@@ -100,16 +100,16 @@ describe.only('Wi-Fi Commands for Protected Devices [@device,@wifi]', () => {
 
 		it('Joins a known Wi-Fi network', async () => {
 			// expect that the network is present in the list
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout: listStdout } = await cli.run(['wifi', 'list']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'join', '--ssid', WIFI_SSID]);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(listStdout).to.include(WIFI_SSID);
 			expect(stripAnsi(stdout)).to.include(`Wi-Fi network '${WIFI_SSID}' configured successfully. Attemping to join...\nUse particle wifi current to check the current network.`);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
@@ -117,38 +117,38 @@ describe.only('Wi-Fi Commands for Protected Devices [@device,@wifi]', () => {
 
 		it('Fetches the current network the device is connected to', async () => {
 			// Let the device join a network and then clear it
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'current']);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(stripAnsi(stdout)).to.include(WIFI_SSID);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
 		});
 
 		it('Lists networks on the device', async () => {
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'list']);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(stripAnsi(stdout)).to.include('List of Wi-Fi networks on the device:');
 			expect(stripAnsi(stdout)).to.include(WIFI_SSID);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
 		});
 
 		it('removes a Wi-Fi network', async () => {
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'remove', '--ssid', WIFI_SSID]);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			const { stdout: listStdout } = await cli.run(['wifi', 'list']);
 
@@ -156,7 +156,7 @@ describe.only('Wi-Fi Commands for Protected Devices [@device,@wifi]', () => {
 			expect(listStdout).to.not.include(WIFI_SSID);
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
@@ -164,19 +164,19 @@ describe.only('Wi-Fi Commands for Protected Devices [@device,@wifi]', () => {
 
 		it('Clears networks from the device', async () => {
 			// Let the device add a network and then clear it
-            const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPBefore } = await cli.run(['device-protection', 'status']);
 			await cli.run(['wifi', 'add', '--file', WIFI_CREDS_FILE]);
 			const { stdout: listStdoutBeforeClearing } = await cli.run(['wifi', 'list']);
 			const { stdout, stderr, exitCode } = await cli.run(['wifi', 'clear']);
 			const { stdout : listStdoutAfterClearing }  = await cli.run(['wifi', 'list']);
-            const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
+			const { stdout: stdoutPAfter } = await cli.run(['device-protection', 'status']);
 
 			expect(stripAnsi(listStdoutBeforeClearing)).to.include(WIFI_SSID);
 			expect(stripAnsi(stdout)).to.include('Wi-Fi networks cleared successfully.');
 			expect(stderr).to.equal('');
 			expect(exitCode).to.equal(0);
 			expect(stripAnsi(listStdoutAfterClearing)).to.not.include(WIFI_SSID);
-            expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
+			expect((stdoutPBefore.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPBefore.split('\n'))[0]).to.not.include('Service Mode');
 			expect((stdoutPAfter.split('\n'))[0]).to.include('Protected Device');
 			expect((stdoutPAfter.split('\n'))[0]).to.not.include('Service Mode');
