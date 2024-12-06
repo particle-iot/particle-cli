30715d5526192c8fd57fd6fc02617c5306c194a2
keeramis <keerthy.amisagadda@gmail.com>
2024-12-06 14:38:32 -0800

    Command to flash Tachyon

--- src/cli/flash.js
+++ src/cli/flash.js
@@ -37,6 +37,14 @@ module.exports = ({ commandProcessor, root }) => {
 			},
 			'port': {
 				describe: 'Use this serial port instead of auto-detecting. Useful if there are more than 1 connected device. Only available for serial'
+			},
+			'tachyon' : {
+				boolean: true,
+				description: 'Flash Tachyon'
+			},
+			'verbose' : {
+				boolean: false,
+				description: 'Enable logging'
 			}
 		},
 		handler: (args) => {
@@ -54,7 +62,8 @@ module.exports = ({ commandProcessor, root }) => {
 			'$0 $command --local application.bin': 'Flash the pre-compiled binary to the device connected over USB',
 			'$0 $command --local application.zip': 'Flash the pre-compiled binary and assets from the bundle to the device connected over USB',
 			'$0 $command --local tinker': 'Flash the default Tinker app to the device connected over USB',
-			'$0 $command --usb firmware.bin': 'Flash the binary over USB'
+			'$0 $command --usb firmware.bin': 'Flash the binary over USB',
+			'$0 $command --tachyon /path/to/unpackaged-tool-and-files': 'Flash Tachyon from the files in the specified directory. Use --verbose to see the progress',
 		},
 		epilogue: unindent(`
 		  When passing the --local flag, Device OS will be updated if the version on the device is outdated.
