186145f3efe444138d65ec10f2cd368d60bc5ffa
Hugo Montero <hugo.montero.ventura@particle.io>
2025-05-27 08:25:27 -0600

    add wifiscanner for macos after airport deprecation

---
+++ src/lib/wifi-scanner.js
@@ -0,0 +1,81 @@
+// TODO (hmontero): Migrate it to node-wifiscanner2
+
+const execa = require('execa');
+const macProvider = '/usr/sbin/system_profiler';
+
+
+async function scan() {
+	const { stdout } = await execa(macProvider, [
+		'SPAirPortDataType',
+		'-detailLevel',
+		'full',
+		'-json'
+	]);
+
+	const data = JSON.parse(stdout);
+	return parseAirportData(data);
+}
+
+async function parseAirportData(systemProfilerAirport) {
+	const airPortData = systemProfilerAirport.SPAirPortDataType[0];
+	const interfaces = airPortData.spairport_airport_interfaces;
+	const en0Interface = interfaces.find(iface => iface._name === 'en0');
+	const networks = en0Interface.spairport_airport_other_local_wireless_networks;
+	// filter repeated networks
+	const uniqueNetworks = filterUniqueNetworks(networks);
+
+	return uniqueNetworks.map(formatNetwork);
+}
+
+function filterUniqueNetworks(networks) {
+	const seen = new Set();
+	return networks.filter(net => {
+		if (!net._name || seen.has(net._name)) {
+			return false;
+		}
+		seen.add(net._name);
+		return true;
+	});
+}
+
+function formatNetwork(network) {
+	const [signal, noise] = (network.spairport_signal_noise || '').split('/').map(v => v.trim());
+	const matchChannel = network.spairport_network_channel?.match(/^(\d+)/);
+	const channel = matchChannel ? matchChannel[1] : '';
+	const security = networkSecurity(network.spairport_security_mode);
+	return {
+		ssid: network._name,
+		mac: '',
+		channel,
+		signal_level: signal || '',
+		noise,
+		security
+	};
+}
+
+function networkSecurity(securityMode) {
+	if (!securityMode) {
+		return 'none';
+	}
+	if (securityMode.includes('_wpa3')) {
+		return 'wpa3';
+	}
+	if (securityMode.includes('_wpa2')) {
+		return 'wpa2';
+	}
+	if (securityMode.includes('_wpa')) {
+		return 'wpa';
+	}
+	if (securityMode.includes('_wep')) {
+		return 'wep';
+	}
+	if (securityMode.includes('_none')) {
+		return 'none';
+	}
+	return 'unknown';
+}
+
+module.exports = {
+	scan
+};
+
