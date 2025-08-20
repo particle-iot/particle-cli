8c1c9543eadeb1fafcbee5139e0ca87e6cdd923d
Julien Vanier <julien@particle.io>
2025-08-19 20:00:58 -0400

    Rely on the LICENSE text at the top of the repo rather than license headers

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -1,31 +1,3 @@
-/**
- ******************************************************************************
- * @file    lib/utilities.js
- * @author  David Middlecamp (david@particle.io)
- * @company Particle ( https://www.particle.io/ )
- * @source https://github.com/spark/particle-cli
- * @version V1.0.0
- * @date    14-February-2014
- * @brief   General Utilities Module
- ******************************************************************************
-Copyright (c) 2016 Particle Industries, Inc.  All rights reserved.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope utilities it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this program; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
- */
-
-
 const fs = require('fs');
 const _ = require('lodash');
 const propertiesParser = require('properties-parser');
