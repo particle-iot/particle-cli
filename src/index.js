bd2b48211a07430bdb3a41c6eed08c8a3e2b81a0
Andrew Zenk <andrew.zenk@particle.io>
2025-08-21 15:59:17 -0500

    Create symlink to credential helper

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const credHelper = require('./docker-credential-helper');
 
 if (hasValidNodeInstall()) {
 	// if called as docker-credential-particle exec that instead
-	if (process.argv[0] === 'docker-credential-particle') {
-		credHelper.runGetCommand();
+	if (process.argv0.includes('docker-credential-particle')) {
+		credHelper.runCommand();
 	} else {
 		new CLI().run(process.argv);
 	}
