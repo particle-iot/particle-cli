c9dec653330d80bed408ecb386c73f75332f8dab
Hugo Montero <hugo.montero.ventura@particle.io>
2025-11-26 16:20:25 -0600

    add env-vars command and list method

---
+++ src/cmd/env-vars.js
@@ -0,0 +1,62 @@
+'use strict';
+const CLICommandBase = require('./base');
+const ParticleAPI = require('./api');
+const settings = require('../../settings');
+
+module.exports = class EnvVarsCommand extends CLICommandBase {
+	constructor(...args) {
+		super(...args);
+		this.api = createAPI();
+	}
+
+	async list({ org, product, device, json }){
+		const envVars = await this.ui.showBusySpinnerUntilResolved('Retrieving Environment Variables...',
+			this.api.listEnvVars({ org, productId: product, deviceId: device }));
+		if (json) {
+			this.ui.write(JSON.stringify(envVars, null, 2));
+		} else {
+			await this._displayEnvVars(envVars);
+		}
+	}
+
+	async _displayEnvVars(envVars) {
+		const envs = envVars.env;
+		const mixedEnvs = {
+			...envs.available, // TODO(hmontero): remove it once is removed from api
+			...envs.inherited
+		};
+		const levelDefinedEnvs = envs.own;
+		const inheritedEnvKeys = Object.keys(mixedEnvs);
+		const levelDefinedEnvKeys = Object.keys(envVars.env.own);
+		inheritedEnvKeys.forEach((key) => {
+			if (levelDefinedEnvs[key]) {
+				this._writeEnvBlock(key, levelDefinedEnvs[key], { isOverride: true });
+			} else {
+				this._writeEnvBlock(key, mixedEnvs[key]);
+			}
+		});
+		levelDefinedEnvKeys
+			.filter(key => !inheritedEnvKeys.includes(key))
+			.forEach((key) => this._writeEnvBlock(key, levelDefinedEnvs[key]));
+	}
+
+	_writeEnvBlock(key, envEntry, { isOverride = false } = {}) {
+		if (!envEntry) {
+			return;
+		}
+
+		const { value, from = 'Owner' } = envEntry;
+
+		this.ui.write(this.ui.chalk.cyan(this.ui.chalk.bold(key)));
+		this.ui.write('---------------------------------------------');
+		this.ui.write(`    Value: ${value}${isOverride ? ' (Override)' : ''}`);
+		this.ui.write(`    Scope: ${from}`);
+		this.ui.write('---------------------------------------------');
+	};
+};
+
+function createAPI() {
+	return new ParticleAPI(settings.apiUrl, {
+		accessToken: settings.access_token
+	});
+}
